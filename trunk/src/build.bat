50bb8ab8356a3c87ff427caec0bb91492c56c830
chadwickj <chadwickj@djnamericas.tfs.local>
2012-04-09 14:11:25 +0000

    Updating build scripts to generate packages locally
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C82246

--- trunk/src/build.bat
+++ trunk/src/build.bat
@@ -1,3 +1,16 @@
-%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /m Framework.sln
+SET MSBUILD=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 
-PAUSE
+SET DefaultBuildDir=C:\Temp\Build
+SET Configuration=Release
+SET FrameworkVersionFile=%DefaultBuildDir%\version.txt
+SET OutDir=%DefaultBuildDir%\%Configuration%
+SET NuGetBasePath=%OutDir%
+SET NuGetOutputPath=%OutDir%\nuget
+
+echo %OutDir%
+
+DEL /F /S /Q %OutDir%
+
+%MSBUILD% /p:Configuration=%Configuration% /p:OutDir="%OutDir%" /m Framework.sln
+
+%MSBUILD% /p:Configuration=%Configuration% /p:DefaultBuildDir="%DefaultBuildDir%" /p:NuGetBasePath="%NuGetBasePath%" /p:OutDir="%OutDir%" /p:FrameworkVersionFile="%FrameworkVersionFile%" /p:NuGetOutputPath="%NuGetOutputPath%" default.proj
