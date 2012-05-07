a74535ae50e6453c8c5527280e075af220379f9c
Dacosta, David <David.Dacosta@dowjones.com>
2012-05-07 20:51:12 +0000

    Changed the attributes for searialization and some cleanup on the video player component.
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C84157

--- trunk/src/DowJones.Web.Mvc.UI.Components.Models/VideoPlayer/ClipCollection.cs
+++ trunk/src/DowJones.Web.Mvc.UI.Components.Models/VideoPlayer/ClipCollection.cs
@@ -5,7 +5,6 @@ using Newtonsoft.Json;
 namespace DowJones.Web.Mvc.UI.Components.Models
 {
     [DataContract(Name = "clipCollection", Namespace = "")]
-    [JsonObject(MemberSerialization.OptIn, Id = "clipCollection")]
     public class ClipCollection : List<Clip>
     {
         public ClipCollection(IEnumerable<Clip> collection) : base(collection) {}
