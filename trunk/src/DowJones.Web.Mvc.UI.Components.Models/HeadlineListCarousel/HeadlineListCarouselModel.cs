83fb125123e91bb062802c8b8c53e33801bee01c
Shetty, Praveen <Praveen.Shetty@dowjones.com>
2012-05-31 21:44:10 +0000

    Added New headline Carousel component
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C85942

---
+++ trunk/src/DowJones.Web.Mvc.UI.Components.Models/HeadlineListCarousel/HeadlineListCarouselModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DowJones.Web.Mvc.UI.Components.HeadlineList;
+using Newtonsoft.Json;
+using DowJones.Converters;
+using DowJones.Web.Mvc.UI.Components.Common.Types;
+using DowJones.Extensions;
+namespace DowJones.Web.Mvc.UI.Components.Models
+{
+    public class HeadlineListCarouselModel : ViewComponentModel
+    {
+        
+        public HeadlineListModel HeadlineList { get; set; }
+        
+        public int NumberOfHeadlinesToScrollBy { get; set; }
+        
+        public string SelectedAccessionNo { get; set; }
+        
+        public string AutoScrollSpeed { get; set; }
+        
+    }
+}
