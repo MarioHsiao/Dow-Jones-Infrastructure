842539350d73da1d6c463b4f4e6da9505e04f1c8
andersonb <andersonb@djnamericas.tfs.local>
2012-06-05 15:33:46 +0000

    news radar component placeholder, model, and showcase example.
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C86273

---
+++ trunk/src/DowJones.Web.Mvc.UI.Components.Models/NewsRadar/NewsEntityModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace DowJones.Web.Mvc.UI.Components.NewsRadar
+{
+    [DataContract(Name = "entity")]
+    public class NewsEntityModel
+    {
+        [DataMember(Name = "entityType")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public EntityType EntityType { get; set; }
+
+        [DataMember(Name = "newsVolumes")]
+        public Collection<NewsVolumeModel> NewsVolumes { get; set; }
+
+        [DataMember(Name = "subjectCode")]
+        public string SubjectCode { get; set; }
+
+        [DataMember(Name = "searchQuery")]
+        public SearchQueryModel SearchQuery { get; set; }
+    }
+}
---
+++ trunk/src/DowJones.Web.Mvc.UI.Components/NewsRadar/NewsRadar.js
@@ -0,0 +1,30 @@
+﻿DJ.UI.NewsRadar = DJ.UI.Component.extend({
+    selectors: {
+
+    },
+
+    options: {
+
+    },
+
+    events: {
+
+    },
+
+    init: function (element, meta) {
+        var $meta = $.extend({ name: "NewsRadar" }, meta);
+
+        // Call the base constructor
+        this._super(element, $meta);
+
+        // call databind if we got data from server
+        $dj.debug('this data', this.data);
+        if (this.data && this.data.resultSet)
+            this.bindOnSuccess(this.data.resultSet);
+    }
+
+});
+
+// Declare this class as a jQuery plugin
+$.plugin('dj_NewsRadar', DJ.UI.NewsRadar);
+$dj.debug('Registered DJ.UI.NewsRadar (extends DJ.UI.Component)');
