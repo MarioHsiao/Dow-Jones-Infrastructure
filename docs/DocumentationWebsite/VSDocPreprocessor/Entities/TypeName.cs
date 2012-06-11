7b071e9b63bbfca1c1767c9aa2a47b4556763eed
chadwickj <chadwickj@djnamericas.tfs.local>
2012-06-11 19:05:58 +0000

    Pretty-printing primitive types
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C86787

--- docs/DocumentationWebsite/VSDocPreprocessor/Entities/TypeName.cs
+++ docs/DocumentationWebsite/VSDocPreprocessor/Entities/TypeName.cs
@@ -1,7 +1,30 @@
-﻿namespace VSDocPreprocessor.Entities
+﻿using System.Collections.Generic;
+
+namespace VSDocPreprocessor.Entities
 {
     public class TypeName
     {
+        private static readonly int SystemNamespace = "System".Length;
+
+        private static readonly IDictionary<string, string> PrimitiveTypes = 
+            new Dictionary<string,string> {
+                    {"SByte", "sbyte"},
+                    {"Byte", "byte"},
+                    {"Int16", "short"},
+                    {"UInt16", "ushort"},
+                    {"Int32", "int"},
+                    {"UInt32", "uint"},
+                    {"Int64", "long"},
+                    {"UInt64", "ulong"},
+                    {"Char", "char"},
+                    {"Single", "float"},
+                    {"Double", "double"},
+                    {"Boolean", "bool"},
+                    {"Decimal", "decimal"},
+                    {"Object", "object"},
+                    {"String", "string"},
+                };
+
         public string Namespace { get; private set; }
         public string Name { get; private set; }
         public string FullName { get; private set; }
@@ -24,6 +47,13 @@
             {
                 Namespace = fullName.Substring(0, lastDot);
                 Name = fullName.Substring(lastDot + 1);
+
+                string name;
+                if(lastDot == SystemNamespace && PrimitiveTypes.TryGetValue(Name, out name))
+                {
+                    Name = name;
+                    Namespace = null;
+                }
             }
             else
             {
