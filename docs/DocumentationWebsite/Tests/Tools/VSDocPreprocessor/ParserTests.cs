bba756085755072d42cc32ad11075a0440a34338
chadwickj <chadwickj@djnamericas.tfs.local>
2012-06-11 17:58:47 +0000

    Adding Tests
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C86779

---
+++ docs/DocumentationWebsite/Tests/Tools/VSDocPreprocessor/ParserTests.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VSDocPreprocessor;
+using VSDocSplitter;
+
+namespace DowJones.Documentation.Tests.Tools.VSDocPreprocessor
+{
+    [TestClass]
+    public class ParserTests
+    {
+        private readonly Parser _parser;
+        private readonly Serializer _serializer;
+
+        public ParserTests()
+        {
+            _parser = new Parser();
+            _serializer = new Serializer();
+        }
+
+        [TestMethod]
+        public void ShouldParseVSDoc()
+        {
+            var vsDoc = LoadFromResource("TestData.Input.Tests.XML");
+            var types = _parser.Parse(vsDoc);
+
+            foreach (var entity in types)
+            {
+                using(var writer = new StringWriter())
+                {
+                    _serializer.Serialize(entity, writer);
+                    var actualSerialized = writer.ToString();
+
+                    var resourcename = string.Format("TestData.Expected.{0}.xml", entity.FullName);
+                    var expected = LoadFromResource(resourcename);
+                    var actual = XDocument.Parse(actualSerialized);
+
+                    Assert.AreEqual(
+                        expected.Document.ToString(),
+                        actual.Document.ToString(),
+                        entity.FullName + " not the same");
+                }
+            }
+        }
+
+        private XDocument LoadFromResource(string resourcename)
+        {
+            var type = GetType();
+            var stream = type.Assembly.GetManifestResourceStream(type, resourcename);
+            var xml = new StreamReader(stream).ReadToEnd();
+            return XDocument.Parse(xml);
+        }
+    }
+}
