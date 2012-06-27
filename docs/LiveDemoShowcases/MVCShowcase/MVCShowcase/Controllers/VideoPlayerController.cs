19d994fb1b7c41dc29e7d3c29fe3ea9d6cbf57f4
Gavara, Dilip <Dilip.Gavara@dowjones.com>
2012-06-27 19:25:00 +0000

    Updated VideoPlayer Styles
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C88189

--- docs/LiveDemoShowcases/MVCShowcase/MVCShowcase/Controllers/VideoPlayerController.cs
+++ docs/LiveDemoShowcases/MVCShowcase/MVCShowcase/Controllers/VideoPlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Configuration;
+using System.Web.Mvc;
 using DowJones.Web.Mvc.UI.Components.Models;
 
 namespace DowJones.MvcShowcase.Controllers
@@ -10,32 +11,31 @@ namespace DowJones.MvcShowcase.Controllers
 
         public ActionResult Index()
         {
+            var appPath = ConfigurationManager.AppSettings["InfrastructureShowcase.BasePath"];
             var mediaContent = new Clip
             {
-                Url = "http://m.wsj.net/video/20120601/060112hubamGOOGLE/060112hubamGOOGLE_1500k.mp4",
+                Url = appPath + "/styles/views/videoplayer/media/demo.mp4",
                 Medium = Medium.Video,
                 Duration = "188",
                 Type =  "video/mp4",
                 BitRate = "1500",
                 FrameRate = "29.97",
                 Width = "450",
-                Height = "170"
+                Height = "200"
             };
             var videoPlayerModel = new VideoPlayerModel
             {
                 AutoPlay = false,
                 Width = 430,
-                Height = 158,
+                Height = 188,
                 PlayList = new ClipCollection(new[] { mediaContent }),
-                ControlBarPath = "http://fdevweb3.win.dowjones.net/widgets/3.0/assets/FlowPlayer/flowplayer.controls-3.2.5.swf",
-                PlayerPath = "http://fdevweb3.win.dowjones.net/widgets/3.0/assets/FlowPlayer/flowplayer.unlimited-3.2.7.swf",
-                RTMPPluginPath = "http://fdevweb3.win.dowjones.net/widgets/3.0/assets/FlowPlayer/flowplayer.rtmp-3.2.3.swf",
-                SplashImagePath = "http://fdevweb3.win.dowjones.net/widgets/3.0/assets/FlowPlayer/play_text_large.png",
+                ControlBarPath = appPath + "/styles/views/videoplayer/img/flowplayer.controls-3.2.5.swf",
+                PlayerPath = appPath + "/styles/views/videoplayer/img/flowplayer.unlimited-3.2.7.swf",
+                RTMPPluginPath = appPath + "/styles/views/videoplayer/img/flowplayer.rtmp-3.2.3.swf",
+                SplashImagePath = appPath + "/styles/views/videoplayer/img/FlowPlayer/play_text_large.png",
                 PlayerKey = "75a6c4404d9ffa80a63",
             };
             return View(videoPlayerModel);
         }
-
-
     }
 }
