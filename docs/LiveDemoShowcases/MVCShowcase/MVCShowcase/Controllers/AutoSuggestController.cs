6abbd82282b0298f0053814ff158851bf3746142
Gavara, Dilip <Dilip.Gavara@dowjones.com>
2012-07-02 14:23:14 +0000

    Updated Autosuggest to one control
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C88476

--- docs/LiveDemoShowcases/MVCShowcase/MVCShowcase/Controllers/AutoSuggestController.cs
+++ docs/LiveDemoShowcases/MVCShowcase/MVCShowcase/Controllers/AutoSuggestController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Configuration;
 using System.Web.Mvc;
 using DowJones.Web.Mvc.UI.Components.Models;
 
@@ -12,29 +11,9 @@ namespace DowJones.MvcShowcase.Controllers
 
         public ActionResult Index()
         {
-            var model = GetAutoSuggestModels();
-            return View("Index", model);
-        }
-
-        public List<AutoSuggestModel> GetAutoSuggestModels()
-        {
             var authTypeToken = ConfigurationManager.AppSettings["SuggestAuthTypeToken"];
             var suggestServiceURl = ConfigurationManager.AppSettings["SuggestServiceUrl"];
-
-            var autoSuggestModels = new List<AutoSuggestModel>();
-            var sourceSuggestModel = new AutoSuggestModel
-            {
-                SuggestServiceUrl = suggestServiceURl,
-                AutocompletionType = "Source",
-                AuthType = "SuggestContext",
-                AuthTypeValue = authTypeToken,
-                ServiceOptions = "{\"types\":\"blog\"}",
-                Tokens = "{\"blogTkn\":\"Blog\"}",
-                ControlId = "djSourceAutoSuggest"
-            };
-            autoSuggestModels.Add(sourceSuggestModel);
-
-            var keywordSuggestModel = new AutoSuggestModel
+            var model = new AutoSuggestModel
             {
                 SuggestServiceUrl = suggestServiceURl,
                 AutocompletionType = "Keyword",
@@ -44,70 +23,7 @@ namespace DowJones.MvcShowcase.Controllers
                 FillInputOnKeyUpDown = true,
                 SelectFirst = true
             };
-            autoSuggestModels.Add(keywordSuggestModel);
-
-            var companySuggestModel = new AutoSuggestModel
-            {
-                SuggestServiceUrl = suggestServiceURl,
-                AutocompletionType = "Company",
-                AuthType = "SuggestContext",
-                AuthTypeValue = authTypeToken,
-                Columns = "value|code|ticker",
-                ControlId = "djCompanyAutoSuggest"
-            };
-            autoSuggestModels.Add(companySuggestModel);
-
-            var executiveSuggestModel = new AutoSuggestModel
-            {
-                SuggestServiceUrl = suggestServiceURl,
-                AutocompletionType = "Executive",
-                AuthType = "SuggestContext",
-                AuthTypeValue = authTypeToken,
-                ControlId = "djExecutiveAutoSuggest"
-            };
-            autoSuggestModels.Add(executiveSuggestModel);
-
-            var authorSuggestModel = new AutoSuggestModel
-            {
-                SuggestServiceUrl = suggestServiceURl,
-                AutocompletionType = "Author",
-                AuthType = "SuggestContext",
-                AuthTypeValue = authTypeToken,
-                ControlId = "djAuthorAutoSuggest"
-            };
-            autoSuggestModels.Add(authorSuggestModel);
-
-            var outletSuggestModel = new AutoSuggestModel
-            {
-                SuggestServiceUrl = suggestServiceURl,
-                AutocompletionType = "Outlet",
-                AuthType = "SuggestContext",
-                AuthTypeValue = authTypeToken,
-                ControlId = "djOutletAutoSuggest"
-            };
-            autoSuggestModels.Add(outletSuggestModel);
-
-            var publisherCitySuggestModel = new AutoSuggestModel
-            {
-                SuggestServiceUrl = suggestServiceURl,
-                AutocompletionType = "publisherCity",
-                AuthType = "SuggestContext",
-                AuthTypeValue = authTypeToken,
-                ControlId = "djPublisherCityAutoSuggest"
-            };
-            autoSuggestModels.Add(publisherCitySuggestModel);
-
-            var publisherMetaDataSuggestModel = new AutoSuggestModel
-            {
-                SuggestServiceUrl = suggestServiceURl,
-                AutocompletionType = "publisherMetaData",
-                AuthType = "SuggestContext",
-                AuthTypeValue = authTypeToken,
-                ControlId = "djPublisherMetaDataAutoSuggest"
-            };
-            autoSuggestModels.Add(publisherMetaDataSuggestModel);
-
-            return autoSuggestModels;
+            return View("Index", model);
         }
     }
 }
