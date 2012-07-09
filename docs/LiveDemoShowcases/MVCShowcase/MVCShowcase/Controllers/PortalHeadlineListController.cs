b1e6893e93001d2a25bd7d6a02a887318817e22e
Panda, Hrusikesh <Hrusikesh.Panda@dowjones.com>
2012-07-09 19:31:45 +0000

    Sample GetData
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C88889

--- docs/LiveDemoShowcases/MVCShowcase/MVCShowcase/Controllers/PortalHeadlineListController.cs
+++ docs/LiveDemoShowcases/MVCShowcase/MVCShowcase/Controllers/PortalHeadlineListController.cs
@@ -1,4 +1,7 @@
 using System.Web.Mvc;
+using DowJones.Ajax.PortalHeadlineList;
+using DowJones.Assemblers.Headlines;
+using DowJones.Formatters.Globalization.DateTime;
 using DowJones.Web.Mvc.UI.Components.Common.Types;
 using DowJones.Web.Mvc.UI.Components.Models;
 
@@ -19,8 +22,27 @@ namespace DowJones.MvcShowcase.Controllers
 				SourceClickable = true,
 				DisplaySnippets = SnippetDisplayType.Hover,
 				Layout = PortalHeadlineListLayout.HeadlineLayout,
+				Result = GetData()
 			};
             return View(model);
         }
+
+
+		private PortalHeadlineListDataResult GetData()
+		{
+			// random feed url
+			const string url = "http://feeds.haacked.com/haacked";
+
+			var headlineListManager = new HeadlineListConversionManager(new DateTimeFormatter("en"));
+
+			// process the feed a get a HeadlineListDataResult
+			var feed = headlineListManager.ProcessFeed(url);
+
+			// map relevant fields from response to portalHeadlineListDataResult
+			var portalHeadlineListDataResult = PortalHeadlineConversionManager.Convert(feed.result);
+
+			// return data
+			return portalHeadlineListDataResult;
+		}
     }
 }
