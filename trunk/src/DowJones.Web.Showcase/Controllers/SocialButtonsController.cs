3455b172ed56edee2a5a1a617abaa9035ffb339b
Gavara, Dilip <Dilip.Gavara@dowjones.com>
2012-07-10 15:48:54 +0000

    Added SocialButtons
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C88948

---
+++ trunk/src/DowJones.Web.Showcase/Controllers/SocialButtonsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using DowJones.Web.Mvc.UI.Components.SocialButtons;
+
+namespace DowJones.Web.Showcase.Controllers
+{
+    public class SocialButtonsController : Controller
+    {
+        //
+        // GET: /SocialButtons/
+
+        public ActionResult Index()
+        {
+            var socialNetworks = new List<SocialNetworks> {SocialNetworks.Delicious, SocialNetworks.Facebook, SocialNetworks.Digg, SocialNetworks.Yahoo, SocialNetworks.Google};
+
+
+            var model = new SocialButtonsModel
+                            {
+                                SocialNetworks = socialNetworks,
+                                ImageSize = ImageSize.Small,
+                                Title = "TestTitle",
+                                Url = "http://www.dowjones.com",
+                                Keywords = "Social",
+                                Description = "Test Description"
+                            };
+            return View(model);
+        }
+
+    }
+}
