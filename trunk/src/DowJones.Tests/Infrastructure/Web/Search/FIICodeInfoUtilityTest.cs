b7be396fde0d5913012fe9d6f4c73124d66267e2
chadwickj <chadwickj@djnamericas.tfs.local>
2012-07-11 17:43:34 +0000

    Removing bad test
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C89045

--- trunk/src/DowJones.Tests/Infrastructure/Web/Search/FIICodeInfoUtilityTest.cs
+++ trunk/src/DowJones.Tests/Infrastructure/Web/Search/FIICodeInfoUtilityTest.cs
@@ -7,21 +7,13 @@ using Factiva.Gateway.Managers;
 
 namespace DowJones.Infrastructure.Web.Search
 {
-    [TestClass]
     public class FIICodeInfoUtilityTest : UnitTestFixture
     {
-        [TestMethod]
         public void GetFIICodes_Test()
         {
-
             ControlData controlData = ControlDataManager.GetLightWeightUserControlData("apichecker", "apichecker", "16");
-
             FIICodeInfoUtility utility = new FIICodeInfoUtility();
             List<FIICodeInfo> fiicodes = utility.GetFIICodesInfo(controlData, new List<string> { "ibm" }, new List<string> { "iacc", "asiaz", "india" }, new List<string> { "iadv" }, null, "en");
-            //List<FIICodeInfo> fiicodes = utility.GetFIICodesInfo(controlData, new List<string> { "ibm" }, null, null, null, "en");
-
-            Console.WriteLine("Response");
-
         }
     }
 }
