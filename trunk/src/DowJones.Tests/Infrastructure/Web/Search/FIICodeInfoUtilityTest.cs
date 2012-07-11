3ab9e772f59bf86bf3feab180ee0d63a28449c64
chadwickj <chadwickj@djnamericas.tfs.local>
2012-07-11 15:38:42 +0000

    Merging from v2.2
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C89027

--- trunk/src/DowJones.Tests/Infrastructure/Web/Search/FIICodeInfoUtilityTest.cs
+++ trunk/src/DowJones.Tests/Infrastructure/Web/Search/FIICodeInfoUtilityTest.cs
@@ -8,19 +8,20 @@ using Factiva.Gateway.Managers;
 namespace DowJones.Infrastructure.Web.Search
 {
     [TestClass]
-    public class FIICodeInfoUtilityTest :UnitTestFixture
+    public class FIICodeInfoUtilityTest : UnitTestFixture
     {
-//        [TestMethod]
+        [TestMethod]
         public void GetFIICodes_Test()
         {
 
             ControlData controlData = ControlDataManager.GetLightWeightUserControlData("apichecker", "apichecker", "16");
 
             FIICodeInfoUtility utility = new FIICodeInfoUtility();
-            List<FIICodeInfo> fiicodes = utility.GetFIICodesInfo(controlData, new List<string> { "iacc", "asiaz", "india" }, new List<string> { "iadv" }, null, "en");
-                                    
+            List<FIICodeInfo> fiicodes = utility.GetFIICodesInfo(controlData, new List<string> { "ibm" }, new List<string> { "iacc", "asiaz", "india" }, new List<string> { "iadv" }, null, "en");
+            //List<FIICodeInfo> fiicodes = utility.GetFIICodesInfo(controlData, new List<string> { "ibm" }, null, null, null, "en");
+
             Console.WriteLine("Response");
-            
+
         }
     }
 }
