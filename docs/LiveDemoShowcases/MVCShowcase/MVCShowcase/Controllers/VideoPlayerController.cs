ff059d9986303baedaa283d1de675ac1ed2db115
Gavara, Dilip <Dilip.Gavara@dowjones.com>
2012-07-11 21:09:17 +0000

    Updated Social Buttons
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C89075

--- docs/LiveDemoShowcases/MVCShowcase/MVCShowcase/Controllers/VideoPlayerController.cs
+++ docs/LiveDemoShowcases/MVCShowcase/MVCShowcase/Controllers/VideoPlayerController.cs
@@ -12,29 +12,33 @@ namespace DowJones.MvcShowcase.Controllers
         public ActionResult Index()
         {
             var appPath = ConfigurationManager.AppSettings["InfrastructureShowcase.BasePath"];
-            var mediaContent = new Clip
-            {
-                Url = appPath + "/styles/views/videoplayer/media/demo.mp4",
-                Medium = Medium.Video,
-                Duration = "188",
-                Type =  "video/mp4",
-                BitRate = "1500",
-                FrameRate = "29.97",
-                Width = "670",
-                Height = "300"
-            };
+            var data = new ClipCollection(new[]
+                                              {
+                                                  new Clip
+                                                      {
+                                                          Url = appPath + "/styles/views/videoplayer/media/demo.mp4",
+                                                          Medium = Medium.Video,
+                                                          Duration = "188",
+                                                          Type = "video/mp4",
+                                                          BitRate = "1500",
+                                                          FrameRate = "29.97",
+                                                          Width = "670",
+                                                          Height = "300"
+                                                      }
+                                              });
             var videoPlayerModel = new VideoPlayerModel
             {
                 AutoPlay = false,
                 Width = 650,
                 Height = 288,
-                PlayList = new ClipCollection(new[] { mediaContent }),
+                PlayList = data,
                 ControlBarPath = appPath + "/styles/views/videoplayer/img/flowplayer.controls-3.2.5.swf",
                 PlayerPath = appPath + "/styles/views/videoplayer/img/flowplayer.unlimited-3.2.7.swf",
                 RTMPPluginPath = appPath + "/styles/views/videoplayer/img/flowplayer.rtmp-3.2.3.swf",
                 SplashImagePath = appPath + "/styles/views/videoplayer/img/FlowPlayer/play_text_large.png",
                 PlayerKey = "75a6c4404d9ffa80a63",
             };
+
             return View(videoPlayerModel);
         }
     }
