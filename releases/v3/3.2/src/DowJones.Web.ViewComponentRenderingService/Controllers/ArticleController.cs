2a4168f540adf6e6efeea6d1a106bb88805bff8a
Gavara, Dilip <Dilip.Gavara@dowjones.com>
2012-07-18 22:15:48 +0000

    Updated packages
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C89487

--- releases/v3/3.2/src/DowJones.Web.ViewComponentRenderingService/Controllers/ArticleController.cs
+++ releases/v3/3.2/src/DowJones.Web.ViewComponentRenderingService/Controllers/ArticleController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Web.Mvc;
 using DowJones.Ajax.Article;
 using DowJones.Extensions;
 using DowJones.Articles;
-using DowJones.Web.Mvc.UI.Components.Models;
-using DowJones.Web.Mvc.UI.Components.Models.Article;
+using DowJones.Web.Mvc.UI.Components.Article;
+using DowJones.Web.Mvc.UI.Components.VideoPlayer;
 using Newtonsoft.Json;
 using ControllerBase = DowJones.Web.Mvc.ControllerBase;
 
@@ -50,10 +49,10 @@ namespace DowJones.Web.ViewComponentRenderingService.Controllers
                     mediaContent = mediaContents[1];
                 }
 
-                model.VideoPlayerModel = new VideoPlayerModel
+                model.VideoPlayer = new VideoPlayerModel
                 {
                     AutoPlay = request.MultimediaPlayerOptions.AutoPlay,
-                    PlayList = new ClipCollection(new[] { Mapper.Map<Clip>(mediaContent) }),
+                    Data = new ClipCollection(new[] { Mapper.Map<Clip>(mediaContent) }),
                     PlayerKey = "75a6c4404d9ffa80a63",
                 };
 
@@ -61,8 +60,8 @@ namespace DowJones.Web.ViewComponentRenderingService.Controllers
                 {
                     if (mediaContent.Medium == "audio")
                     {
-                        model.VideoPlayerModel.Width = request.MultimediaPlayerOptions.AvailableWidth > 0 ? request.MultimediaPlayerOptions.AvailableWidth : 300;
-                        model.VideoPlayerModel.Height = 30;
+                        model.VideoPlayer.Width = request.MultimediaPlayerOptions.AvailableWidth > 0 ? request.MultimediaPlayerOptions.AvailableWidth : 300;
+                        model.VideoPlayer.Height = 30;
                     }
                     else
                     {
@@ -75,35 +74,35 @@ namespace DowJones.Web.ViewComponentRenderingService.Controllers
                             if (mediaContentWidth > request.MultimediaPlayerOptions.AvailableWidth)
                             {
                                 // media dimensions are bigger than available space, resize keeping ratio
-                                model.VideoPlayerModel.Width = request.MultimediaPlayerOptions.AvailableWidth;
-                                model.VideoPlayerModel.Height = (request.MultimediaPlayerOptions.AvailableWidth * mediaContentHeight) / mediaContentWidth;
+                                model.VideoPlayer.Width = request.MultimediaPlayerOptions.AvailableWidth;
+                                model.VideoPlayer.Height = (request.MultimediaPlayerOptions.AvailableWidth * mediaContentHeight) / mediaContentWidth;
                             }
                             else
                             {
-                                model.VideoPlayerModel.Width = mediaContentWidth;
-                                model.VideoPlayerModel.Height = mediaContentHeight;
+                                model.VideoPlayer.Width = mediaContentWidth;
+                                model.VideoPlayer.Height = mediaContentHeight;
                             }
                         }
                     }
 
                     if (!request.MultimediaPlayerOptions.ControlBarPath.IsNullOrEmpty())
                     {
-                        model.VideoPlayerModel.ControlBarPath = request.MultimediaPlayerOptions.ControlBarPath;
+                        model.VideoPlayer.ControlBarPath = request.MultimediaPlayerOptions.ControlBarPath;
                     }
 
                     if (!request.MultimediaPlayerOptions.PlayerPath.IsNullOrEmpty())
                     {
-                        model.VideoPlayerModel.PlayerPath = request.MultimediaPlayerOptions.PlayerPath;
+                        model.VideoPlayer.PlayerPath = request.MultimediaPlayerOptions.PlayerPath;
                     }
 
                     if (!request.MultimediaPlayerOptions.RTMPPluginPath.IsNullOrEmpty())
                     {
-                        model.VideoPlayerModel.RTMPPluginPath = request.MultimediaPlayerOptions.RTMPPluginPath;
+                        model.VideoPlayer.RTMPPluginPath = request.MultimediaPlayerOptions.RTMPPluginPath;
                     }
 
                     if (!request.MultimediaPlayerOptions.SplashImagePath.IsNullOrEmpty())
                     {
-                        model.VideoPlayerModel.SplashImagePath = request.MultimediaPlayerOptions.SplashImagePath;
+                        model.VideoPlayer.SplashImagePath = request.MultimediaPlayerOptions.SplashImagePath;
                     }
                 }
             }
---
+++ releases/v3/3.2/src/DowJones.Web.ViewComponentRenderingService/packages/Factiva.Gateway.Resources.7.5.0.90/content/GatewayResources/xslt/track/RealTimeAlerts_GetFolderResponse.xslt
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:msxsl="urn:schemas-microsoft-com:xslt" xmlns:user="user" extension-element-prefixes="msxsl user" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <xsl:output method="xml" version="1.0" encoding="UTF-8" indent="yes"/>
+  <msxsl:script language="JScript" implements-prefix="user">
+    <![CDATA[		
+		function RemoveXSI(strOrig)
+		{
+			var RetVal = new String("");
+			var temp = strOrig.Replace("<StructuredSearch", "<StructuredSearch xmlns=\"http://types.factiva.com/search\"");
+			// NN - Old code - .replace is invalid anyway so the line below does not execute
+			// RetVal = temp.replace ("xsi:nil=\"true\"", "");			
+			// NN - New code - removes all nodes with xsi:nil="true"
+			RetVal = temp.replace(/<\w+\s+xsi:nil=\"true\"\s*\/>/g,"");
+			return RetVal;			
+		}
+		]]>
+  </msxsl:script>
+  <xsl:template match="/" xmlns="http://types.dowjones.net/foldermanager">
+    <GetFolderResponse>
+      <folderInfo>
+        <xsl:for-each select="//rta:GetFolderResponse/rta:folderInfo/child::*" xmlns:rta="http://types.dowjones.net/foldermanager">
+          <xsl:choose>
+            <xsl:when test="local-name() = 'userQuery'">
+              <xsl:value-of select="user:RemoveXSI(string(.))" disable-output-escaping="yes" />
+            </xsl:when>
+            <xsl:otherwise>
+              <xsl:copy-of select="."/>
+            </xsl:otherwise>
+          </xsl:choose>
+        </xsl:for-each>
+      </folderInfo>
+    </GetFolderResponse>
+  </xsl:template>
+</xsl:stylesheet>
