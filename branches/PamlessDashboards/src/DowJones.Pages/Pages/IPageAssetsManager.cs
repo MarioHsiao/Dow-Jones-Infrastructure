15d1f4dbfefb937c7c43e4a3ea0246010e0d1dd4
chadwickj <chadwickj@djnamericas.tfs.local>
2012-07-19 14:30:56 +0000

    Cleaning up order of methods
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C89513

--- branches/PamlessDashboards/src/DowJones.Pages/Pages/IPageAssetsManager.cs
+++ branches/PamlessDashboards/src/DowJones.Pages/Pages/IPageAssetsManager.cs
@@ -20,25 +20,6 @@ namespace DowJones.Pages
 {
     public interface IPageAssetsManager : IAggregationManager
     {
-        MetaData GetModuleMetaData(GWModule module);
-        MetaData GetModuleMetaData(Modules.ModuleType moduleType);
-        IEnumerable<ModuleIdByMetadata> GetModulesByModuleType(Modules.ModuleType moduleType);
-        IEnumerable<ModuleIdByMetadata> GetModulesByModuleType(Modules.ModuleType moduleType, MetaDataType metaDataType, List<MetaData> metaDataCollection = null);
-        IEnumerable<PageListInfo> GetUserPageList(IEnumerable<PageType> pageTypes, SortOrder sortOrder, SortBy sortBy, bool forceCacheRefresh = false);
-        GetPagesListWithPageResponse GetUserPageListWithDefaultPage(IEnumerable<PageType> pageTypes, SortOrder sortOrder, SortBy sortBy, int pageId, PageDefaultBy pageDefaultedBy = PageDefaultBy.Position, int pagePosition = 1);
-        int ExtractPageId(string pageRef);
-        int ExtractModuleId(string moduleId);
-        PageListInfoExCollection GetSubscribablePages(PageType pageType, IEnumerable<Factiva.Gateway.Messages.Assets.Pages.V1_0.AccessQualifier> accessQualifiers, MetadataFilter metadataFilter);
-        void SetPageShareProperties(string pageRef, ShareProperties shareProperties);
-        void MakePageModulesPublic(IEnumerable<GWModule> moduleCollection);
-        void MakePageModulesPrivate(IEnumerable<GWModule> moduleCollection);
-        //bool CachingEnabled { get; set; }
-        AccessControlScope GetRootAccessControlScope(int pageId, ShareProperties properties, Factiva.Gateway.Messages.Assets.Pages.V1_0.AccessQualifier accessQualifier);
-        PageRef ExtractPageRef(string pageRef, bool forceCacheRefresh = false);
-        EnableDisable EnableDisablePage(string pageRef, EnableDisable action = EnableDisable.Enable);
-        List<KeyValuePair<string, string>> GetAdminPageOwner(int pageId);
-        GWModule GetModuleById(string moduleId);
-        void UpdatePage(GWPage page);
         void AddModuleIdsToPage(string pageRef, IEnumerable<string> moduleIds);
         void AddModuleIdsToEndOfPage(string pageRef, IEnumerable<string> moduleIds);
         void AddModuleIdsToPage(string pageRef, IEnumerable<KeyValuePair<int, int>> moduleIdWithPositionCollection);
@@ -52,17 +33,38 @@ namespace DowJones.Pages
         void DeleteModules(string pageId, IEnumerable<string> moduleIds);
         void DeletePage(string pageId);
         void DeletePageFromSessionCache(string cacheKey, Factiva.Gateway.Messages.Cache.SessionCache.V1_0.CacheScope cacheScope);
+        void DeleteQueryFilter(string id, AssetType assetType);
+        int ExtractPageId(string pageRef);
+        int ExtractModuleId(string moduleId);
+        PageRef ExtractPageRef(string pageRef, bool forceCacheRefresh = false);
+        EnableDisable EnableDisablePage(string pageRef, EnableDisable action = EnableDisable.Enable);
+        MetaData GetModuleMetaData(GWModule module);
+        MetaData GetModuleMetaData(Modules.ModuleType moduleType);
+        IEnumerable<ModuleIdByMetadata> GetModulesByModuleType(Modules.ModuleType moduleType);
+        IEnumerable<ModuleIdByMetadata> GetModulesByModuleType(Modules.ModuleType moduleType, MetaDataType metaDataType, List<MetaData> metaDataCollection = null);
+        AccessControlScope GetRootAccessControlScope(int pageId, ShareProperties properties, Factiva.Gateway.Messages.Assets.Pages.V1_0.AccessQualifier accessQualifier);
+        PageListInfoExCollection GetSubscribablePages(PageType pageType, IEnumerable<Factiva.Gateway.Messages.Assets.Pages.V1_0.AccessQualifier> accessQualifiers, MetadataFilter metadataFilter);
+        IEnumerable<PageListInfo> GetUserPageList(IEnumerable<PageType> pageTypes, SortOrder sortOrder, SortBy sortBy, bool forceCacheRefresh = false);
+        GetPagesListWithPageResponse GetUserPageListWithDefaultPage(IEnumerable<PageType> pageTypes, SortOrder sortOrder, SortBy sortBy, int pageId, PageDefaultBy pageDefaultedBy = PageDefaultBy.Position, int pagePosition = 1);
+        List<KeyValuePair<string, string>> GetAdminPageOwner(int pageId);
+        GWModule GetModuleById(string moduleId);
         GWModule GetModuleById(string pageId, string moduleId);
         GWModule GetModuleByIdWithPage(string pageId, string moduleId, out GWPage page);
         IEnumerable<MetaData> GetModulesNameAndDescriptions(IEnumerable<string> codes);
         GWPage GetPage(string pageId, bool cachePage, bool forceCacheRefresh);
         string GetPageName(string pageId);
         PageListInfoCollection GetPageListInfoCollection(IEnumerable<PageType> pageTypes, SortOrder sortOrder, SortBy sortBy);
+        void SetPageShareProperties(string pageRef, ShareProperties shareProperties);
+        void MakePageModulesPublic(IEnumerable<GWModule> moduleCollection);
+        void MakePageModulesPrivate(IEnumerable<GWModule> moduleCollection);
         void MakePersonalAlertsPublic(IEnumerable<int> alertIds);
         void PublishPage(string pageId, IEnumerable<int> personalAlertIds);
         bool RemoveAssignedPage(string pageId);
         string ReplaceModuleOnPage(string pageId, string rootIDOfNewModule, GWModule moduleToReplaceWith, string moduleIdToRemove);
+        void SetRootModuleId(string moduleId, string rootModuleId);
         string SubscribeToPage(string pageId, int pagePosition);
+        void UnpublishPage(string pageId);
+        void UnsubscribeToPage(string pageId);
         void UpdateModule(string pageId, string moduleId, IEnumerable<Property> properties);
         void UpdateModule(ModuleEx module);
         void UpdateModulePositionsOnPage(string pageId, IEnumerable<IEnumerable<int>> zones);
@@ -71,11 +73,8 @@ namespace DowJones.Pages
         void UpdateModule(GWModule module, IEnumerable<Property> properties);
         void UpdatePagePositions(IEnumerable<PagePosition> pagePositions);
         void UpdateTitleDescription(Page page);
-        void UnpublishPage(string pageId);
-        void UnsubscribeToPage(string pageId);
-        void SetRootModuleId(string moduleId, string rootModuleId);
         void UpdateQueryFilter(string id, AssetType assetType, QueryFilters queryFilters);
-        void DeleteQueryFilter(string id, AssetType assetType);
+        void UpdatePage(GWPage page);
     }
 
     public static class IPageAssetsManagerExtensions
