45192456c73ed84cb695bfa78496b492d9536b5c
Gavara, Dilip <Dilip.Gavara@dowjones.com>
2012-07-20 18:48:39 +0000

    Updated Autosuggest Component
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C89645

---
+++ trunk/src/DowJones.Web.Mvc.UI.Components/AutoSuggest/EmbeddedResources.cs
@@ -0,0 +1,6 @@
+﻿namespace DowJones.Web.Mvc.UI.Components.AutoSuggest
+{
+    [ScriptResourceAttribute("autocomplete-plugin", ResourceName = "DowJones.Web.Mvc.UI.Components.AutoSuggest.scripts.jquery.autocomplete.js", ResourceKind = ClientResourceKind.Script, DeclaringType = typeof(EmbeddedResources))]
+    [ScriptResourceAttribute("jquery-jsonp-plugin", ResourceName = "DowJones.Web.Mvc.UI.Components.AutoSuggest.scripts.jquery.jsonp-2.3.1.min.js", ResourceKind = ClientResourceKind.Script, DeclaringType = typeof(EmbeddedResources))]
+    public static class EmbeddedResources {}
+}
