bc3b74be0a666f620cef147bbc7a8a77801406c5
chadwickj <chadwickj@djnamericas.tfs.local>
2012-07-26 16:56:49 +0000

    Fixing broken stylesheet reference
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C89989

--- trunk/src/DowJones.Web.Mvc.UI.Components/Pager/PagerExtensions.cs
+++ trunk/src/DowJones.Web.Mvc.UI.Components/Pager/PagerExtensions.cs
@@ -1,39 +1,25 @@
-﻿using System.Reflection;
-using DowJones.Web;
+﻿using DowJones.Web;
 using DowJones.Web.Mvc.UI.Components.Pager;
 
 [assembly: System.Web.UI.WebResourceAttribute(PagerExtensions.PagerScriptResourceName, KnownMimeTypes.JavaScript)]
-[assembly: System.Web.UI.WebResourceAttribute(PagerExtensions.PagerStylesheetResourceName, KnownMimeTypes.Stylesheet, PerformSubstitution = true)]
-[assembly: System.Web.UI.WebResourceAttribute("DowJones.Web.Mvc.UI.Components.Pager.images.nav_dots.png", KnownMimeTypes.PngImage)]
 
 namespace DowJones.Web.Mvc.UI.Components.Pager
 {
-
     [ScriptResource(Name = "Pager",  ResourceName = PagerScriptResourceName, DependencyLevel = ClientResourceDependencyLevel.MidLevel)]
 
     public static class PagerExtensions
     {
-        private static readonly Assembly TargetAssembly = typeof(PagerExtensions).Assembly;
         internal const string PagerScriptResourceName = "DowJones.Web.Mvc.UI.Components.Pager.Pager.js";
-        internal const string PagerStylesheetResourceName = "DowJones.Web.Mvc.UI.Components.Pager.Pager.css";
 
         public static ViewComponentFactory Pager(this ViewComponentFactory factory)
         {
             factory.ScriptRegistry().IncludeResource(
-                    TargetAssembly,
+                    typeof(PagerExtensions).Assembly,
                     PagerScriptResourceName,
-                    ClientResourceDependencyLevel.MidLevel
-                );
-
-            factory.StylesheetRegistry().IncludeResource(
-                    TargetAssembly,
-                    PagerStylesheetResourceName,
                     ClientResourceDependencyLevel.MidLevel
                 );
 
             return factory;
         }
-
     }
-
 }
