7e4bf10ce52a0c081010161f4ca0f41817a0b4c4
chadwickj <chadwickj@djnamericas.tfs.local>
2012-08-07 21:25:38 +0000

    Adding Canvas Controller
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C90667

---
+++ branches/Degreasing/src/DowJones.Web.Mvc.UI.Canvas.GatewayFree/Controllers/CanvasController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using DowJones.Web.Mvc.Routing;
+using DowJones.Web.Mvc.UI.Canvas.Controllers;
+
+namespace DowJones.Web.Mvc.UI.Canvas.GatewayFree.Controllers
+{
+    public class CanvasController : PageControllerBase
+    {
+        [JsonModelBinder]
+        public class UpdateModulePositionsRequest
+        {
+            public string PageId { get; set; }
+            public List<int[]> Columns { get; set; }
+        }
+
+        [Route("canvas/modules/positions")]
+        [Route("canvas/modules/positions/{method}")]
+        public ActionResult UpdateModulePositions(UpdateModulePositionsRequest request)
+        {
+            return Json(request, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
