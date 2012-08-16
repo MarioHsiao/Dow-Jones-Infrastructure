583ddd375e349585860898468b273dfd6f49a71e
Panda, Hrusikesh <Hrusikesh.Panda@dowjones.com>
2012-08-16 15:54:05 +0000

    Recreating ComponentRendering - first cut
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C91122

---
+++ ComponentRenderingService/trunk/ComponentRenderingService/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DowJones.Web.ComponentRenderingService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+			// Just return that we are alive
+			return new HttpStatusCodeResult(200, "Working!");
+        }
+
+    }
+}
---
+++ ComponentRenderingService/trunk/ComponentRenderingService/Routes.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using DowJones.Infrastructure;
+
+namespace DowJones.Web.ComponentRenderingService
+{
+	public class Routes : IBootstrapperTask
+	{
+		public void Execute()
+		{
+			RouteTable.Routes.IgnoreRoute("common.js");
+			RouteTable.Routes.MapRoute(
+				"Default",
+				"{controller}/{action}/{id}",
+				new { controller = "Home", action = "Index", id = string.Empty }
+			);
+		}
+	}
+}
