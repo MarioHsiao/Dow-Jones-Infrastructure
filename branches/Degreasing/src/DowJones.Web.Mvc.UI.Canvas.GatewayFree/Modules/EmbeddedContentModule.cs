75e5a7111e4dada0de50a32fa3fc943b1d97d819
chadwickj <chadwickj@djnamericas.tfs.local>
2012-08-21 16:45:29 +0000

    Cleanup
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C91379

--- branches/Degreasing/src/DowJones.Web.Mvc.UI.Canvas.GatewayFree/Modules/EmbeddedContentModule.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DowJones.Web.Mvc.UI.Canvas.GatewayFree.Modules
-{
-    public class EmbeddedContentModule : Pages.Modules.Module
-    {
-        public int? Height { get; set; }
-        public int? Width { get; set; }
-        public string Url { get; set; }
-    }
-}
