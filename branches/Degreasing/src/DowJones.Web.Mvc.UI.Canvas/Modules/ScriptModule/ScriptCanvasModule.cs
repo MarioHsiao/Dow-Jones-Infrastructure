3c5efaa6c36172cfa16b2271d69a1da071bcadf1
chadwickj <chadwickj@djnamericas.tfs.local>
2012-08-28 19:55:36 +0000

    Adding Script Template ID to script module data
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C91692

--- branches/Degreasing/src/DowJones.Web.Mvc.UI.Canvas/Modules/ScriptModule/ScriptCanvasModule.cs
+++ branches/Degreasing/src/DowJones.Web.Mvc.UI.Canvas/Modules/ScriptModule/ScriptCanvasModule.cs
@@ -22,7 +22,7 @@ namespace DowJones.Web.Mvc.UI.Canvas.Modules.ScriptModule
     using System.Web.UI;
     using DowJones.Web.Mvc.Extensions;
     
-    // Last Generated Timestamp: 08/28/2012 11:35 AM
+    // Last Generated Timestamp: 08/28/2012 03:53 PM
     [DowJones.Web.ScriptResourceAttribute(null, ResourceName="DowJones.Web.Mvc.UI.Canvas.Modules.ScriptModule.ScriptModule.js", ResourceKind=DowJones.Web.ClientResourceKind.Script, DeclaringType=typeof(DowJones.Web.Mvc.UI.Canvas.Modules.ScriptModule.ScriptCanvasModule))]
     [System.CodeDom.Compiler.GeneratedCodeAttribute("RazorViewComponentClassGenerator", "1.0.0.27108")]
     public class ScriptCanvasModule : AbstractCanvasModule<Modules.ScriptModule.ScriptModule>
@@ -42,6 +42,11 @@ namespace DowJones.Web.Mvc.UI.Canvas.Modules.ScriptModule
         public override void ExecuteTemplate()
         {
 
+
+
+
+  
+    HtmlAttributes.Add("data-script-template-id", Model.TemplateId);
 
 
 
