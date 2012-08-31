e1d9dea75052d356f559ab59f7d1df686a0710e3
Panda, Hrusikesh <Hrusikesh.Panda@dowjones.com>
2012-08-31 21:37:28 +0000

    Illegal use of template keyword.
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C91923

--- trunk/src/DowJones.Web.Mvc.UI.Components/VideoPlayer/js/VideoPlayer.js
+++ trunk/src/DowJones.Web.Mvc.UI.Components/VideoPlayer/js/VideoPlayer.js
@@ -7,7 +7,7 @@
         autoPlay: false
     },
 
-    templates: { playList: '<a href="${url}"><div>${title}</div><span><em>${duration} min</em></span></a>' },
+    templateMarkups: { playList: '<a href="${url}"><div>${title}</div><span><em>${duration} min</em></span></a>' },
 
     events: {
         onFullScreenExit: 'fullScreenExit.dj.videoPlayer',
@@ -131,7 +131,7 @@
                     var playListItems = $(this.selectors.playListItems, playListWrap);
                     this.entriesId = this.playerId + "_entries";
 
-                    playListEntries.attr("id", this.entriesId).empty().css({ "left": "0px" }).html(this.templates.playList);
+                    playListEntries.attr("id", this.entriesId).empty().css({ "left": "0px" }).html(this.templateMarkups.playList);
                     playListContainer.show();
 
                     this.fPlayer = $f(
