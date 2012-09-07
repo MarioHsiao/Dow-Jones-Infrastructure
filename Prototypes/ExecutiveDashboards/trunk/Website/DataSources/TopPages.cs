80bebaab6b92858d6f1b7a4e55cdf95d2ca4c2b0
chadwickj <chadwickj@djnamericas.tfs.local>
2012-09-07 18:25:28 +0000

    Async-ing Top Pages
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C92247

--- Prototypes/ExecutiveDashboards/trunk/Website/DataSources/TopPages.cs
+++ Prototypes/ExecutiveDashboards/trunk/Website/DataSources/TopPages.cs
@@ -14,47 +14,77 @@ namespace DowJones.Dash.Website.DataSources
 {
     public class TopPages : DataSource
     {
-        public TopPages()
+        public int ErrorDelaySeconds { get; set; }
+        public int PollDelaySeconds { get; set; }
+
+        public TopPages() : this(2)
         {
+        }
+
+        public TopPages(int pollDelay, int? errorDelay = null)
+        {
+            PollDelaySeconds = pollDelay;
+            ErrorDelaySeconds = errorDelay.GetValueOrDefault(pollDelay * 2);
+
             Task.Factory.StartNew(Poll);
         }
 
         protected internal void Poll()
         {
-            while(true)
-            {
-                var data = GetData();
-                OnDataReceived(data);
-                Thread.Sleep(2000);
-            }
-        }
-
-        private object GetData()
-        {
             try
             {
-                var request = WebRequest.Create("http://api.chartbeat.com/toppages/?host=online.wsj.com&limit=5&apikey=9afde3c7a533572878d27e80cf001244");
-                using(var response = request.GetResponse())
-                using(var stream = new StreamReader(response.GetResponseStream()))
-                {
-                    var source = JsonConvert.DeserializeObject<IEnumerable<dynamic>>(stream.ReadToEnd());
-                    var pages =
-                        from page in source
-                        select new PortalHeadlineInfo
-                            {
-                                Title = page.i,
-                                HeadlineUrl = "http://" + page.path,
-                                ModificationTimeDescriptor = page.visitors,
-                            };
-
-                    return new PortalHeadlineListModel { Result = new PortalHeadlineListDataResult(new PortalHeadlineListResultSet(pages)) };
-                }
+                WebRequest
+                    .Create("http://api.chartbeat.com/toppages/?host=online.wsj.com&limit=5&apikey=9afde3c7a533572878d27e80cf001244")
+                    .GetResponseAsync()
+                    .ContinueWith(task => OnResponse(task.Result));
             }
             catch (Exception ex)
             {
-                Trace.TraceError("TopPages request failed: {0}", ex);
-                return null;
+                OnError(ex);
             }
+        }
+
+        private void OnResponse(WebResponse response)
+        {
+            try
+            {
+                using(var stream = response.GetResponseStream())
+                {
+                    var pages = Deserialize(stream);
+                    OnDataReceived(pages);
+                }
+
+                Thread.Sleep(PollDelaySeconds * 1000);
+                Poll();
+            }
+            catch (Exception ex)
+            {
+                OnError(ex);
+            }
+        }
+
+        private void OnError(Exception ex = null)
+        {
+            Trace.TraceError("TopPages request failed: {0}", ex);
+            Thread.Sleep(ErrorDelaySeconds * 1000);
+            Poll();
+        }
+
+        private static PortalHeadlineListModel Deserialize(Stream stream)
+        {
+            var json = new StreamReader(stream).ReadToEnd();
+            var source = JsonConvert.DeserializeObject<IEnumerable<dynamic>>(json);
+            var pages =
+                from page in source
+                select new PortalHeadlineInfo
+                    {
+                        Title = page.i,
+                        HeadlineUrl = "http://" + page.path,
+                        ModificationTimeDescriptor = page.visitors,
+                    };
+
+            return new PortalHeadlineListModel
+                {Result = new PortalHeadlineListDataResult(new PortalHeadlineListResultSet(pages))};
         }
     }
 }
