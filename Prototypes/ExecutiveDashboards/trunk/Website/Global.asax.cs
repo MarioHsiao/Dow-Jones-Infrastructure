e738915915ed40d8b6c5131e195a133d30b05768
chadwickj <chadwickj@djnamericas.tfs.local>
2012-09-10 14:10:46 +0000

    Adding deployment info to layout
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C92327

--- Prototypes/ExecutiveDashboards/trunk/Website/Global.asax.cs
+++ Prototypes/ExecutiveDashboards/trunk/Website/Global.asax.cs
@@ -1,6 +1,16 @@
-﻿namespace DowJones.Dash.Website
+﻿using System;
+
+namespace DowJones.Dash.Website
 {
     public partial class MvcApplication
     {
+/*
+        public static string Version
+        {
+            get { return _version.Value; }
+        }
+*/
+        public static Lazy<string> Version = 
+            new Lazy<string>(typeof(MvcApplication).Assembly.GetName().Version.ToString);
     }
 }
