a622dc43c56e2d6b32e71f1f2736305e9202304e
Dacosta, David <David.Dacosta@dowjones.com>
2012-09-12 02:05:10 +0000

    Added initial implementation of Top Referrers
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C92447

---
+++ Prototypes/ExecutiveDashboards/trunk/DowJones.Dash.Components.Models/TopReferrer/TopReferrer.cs
@@ -0,0 +1,9 @@
+using DowJones.Web.Mvc.UI;
+
+namespace DowJones.Dash.Components.Models.TopReferrer
+{
+	public class TopReferrerModel : ViewComponentModel
+	{
+		// TODO:  Add properties
+	}
+}
---
+++ Prototypes/ExecutiveDashboards/trunk/DowJones.Dash.Components/TopReferrer/TopReferrer.js
@@ -0,0 +1,77 @@
+/*!
+ * TopReferrer
+ */
+
+DJ.UI.TopReferrer = DJ.UI.CompositeComponent.extend({
+
+    selectors: {
+        portalHeadlineListContainer: '.portalHeadlineListContainer'
+    },
+
+    init: function (element, meta) {
+        this._super(element, $.extend({ name: "TopReferrer" }, meta));
+        
+        this._initPortalHeadlines();
+    },
+
+    _initPortalHeadlines: function () {
+        var self = this;
+        DJ.add('PortalHeadlineList', {
+            container: this._portalHeadlinesContainer[0],
+            options: { layout: 2 }
+        }).done(function (comp) {
+            self.portalHeadlines = comp;
+            comp.owner = self;
+        });
+    },
+
+    _initializeDelegates: function () {
+        this._delegates = $.extend(this._delegates, {
+            setData: $dj.delegate(this, this.setData)
+        });
+    },
+
+    _initializeElements: function () {
+        this.$element.html(this.templates.container());
+        this._portalHeadlinesContainer = this.$element.find(this.selectors.portalHeadlineListContainer);
+    },
+
+    _initializeEventHandlers: function () {
+        $dj.subscribe('data.Referrers', this._delegates.setData);
+    },
+    
+    _setData: function (data) {
+        if (!this.portalHeadlines) {
+            $dj.error("PortalHeadlinesComponent is not initialized. Refresh the page to try again.");
+            return;
+        }
+
+        console.log(data);
+        var headlines = [];
+        for (var prop in data.referrers)
+            if (prop.length > 0) {
+                console.log(prop);
+                headlines[headlines.length] = { title: prop, modificationTimeDescriptor: data.referrers[prop] };
+            }
+        /*var headlines = _.map(data, function (page) {
+            return {
+                title: page.i,
+                headlineUrl: "http://" + page.path,
+                modificationTimeDescriptor: page.visitors.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            };
+        });*/
+
+        var result = {
+            count: { value: headlines.length },
+            headlines: headlines
+        };
+
+        this.portalHeadlines.setData({ resultSet: result });
+    },
+    
+    EOF: null
+});
+
+
+// Declare this class as a jQuery plugin
+$.plugin('dj_TopReferrer', DJ.UI.TopReferrer);
