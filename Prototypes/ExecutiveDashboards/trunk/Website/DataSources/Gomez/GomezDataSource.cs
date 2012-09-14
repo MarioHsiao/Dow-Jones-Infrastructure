133c7bf6622a1be50bfa621f21a804aa68c45cf2
Dacosta, David <David.Dacosta@dowjones.com>
2012-09-14 17:29:10 +0000

    updated the connection string
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C92649

--- Prototypes/ExecutiveDashboards/trunk/Website/DataSources/Gomez/GomezDataSource.cs
+++ Prototypes/ExecutiveDashboards/trunk/Website/DataSources/Gomez/GomezDataSource.cs
@@ -4,7 +4,7 @@ namespace DowJones.Dash.Website.DataSources
     {
         private static string GomezConnectionString
         {
-            get { return "Data Source=10.0.64.14;Initial Catalog=master;User Id=webuser;Password=webpwd;"; }
+            get { return "Data Source=10.0.64.14;Initial Catalog=master;User Id=webuser;Password=webpwd;Asynchronous Processing=True"; }
         }
 
         public GomezDataSource(string query = null)
