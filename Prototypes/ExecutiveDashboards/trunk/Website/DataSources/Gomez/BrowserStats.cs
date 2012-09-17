52d6dd286e66f79cf452f39bc9ba9f8478e954db
Dacosta, David <David.Dacosta@dowjones.com>
2012-09-17 17:45:01 +0000

    Fixed some data issues
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C92744

--- Prototypes/ExecutiveDashboards/trunk/Website/DataSources/Gomez/BrowserStats.cs
+++ Prototypes/ExecutiveDashboards/trunk/Website/DataSources/Gomez/BrowserStats.cs
@@ -6,7 +6,7 @@ namespace DowJones.Dash.Website.DataSources.Gomez
     {
         public void Initialize()
         {
-            Query = @"exec [SplunkExport].[dbo].[GetPageLoadHistoricalDetails] @pageid = 421139, @days = 1";
+            Query = @"exec [SplunkExport].[dbo].[GetPageLoadDetailsByBrowser] @pageid = 421139, @seconds = 300 ";
         }
     }
 }
---
+++ Prototypes/ExecutiveDashboards/trunk/Website/DataSources/Gomez/PageLoadHistoricalDetails.cs
@@ -0,0 +1,12 @@
+﻿using DowJones.DependencyInjection;
+
+namespace DowJones.Dash.Website.DataSources.Gomez
+{
+    public class PageLoadHistoricalDetails : GomezDataSource, IInitializable
+    {
+        public void Initialize()
+        {
+            Query = @"exec [SplunkExport].[dbo].[GetPageLoadHistoricalDetails] @pageid = 421139, @days = 1";
+        }
+    }
+}
