30f0680122d63080718a06a759854d3821ea4e9b
Panda, Hrusikesh <Hrusikesh.Panda@dowjones.com>
2012-09-17 21:07:49 +0000

    Updated with gomez data mapping
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C92769

--- Prototypes/ExecutiveDashboards/trunk/DowJones.Dash.Components/BrowserStats/BrowserStats.js
+++ Prototypes/ExecutiveDashboards/trunk/DowJones.Dash.Components/BrowserStats/BrowserStats.js
@@ -28,29 +28,19 @@ DJ.UI.BrowserStats = DJ.UI.Component.extend({
     },
 
     _initializeEventHandlers: function () {
-        $dj.subscribe('data.Browser', this._delegates.setData);
+        $dj.subscribe('data.BrowserStats', this._delegates.setData);
     },
 
     _setData: function (data) {
-        
-        var self = this;
-        var browserData = _.map(data, function (item) {
-            var browser = self._parseBrowserInfo(item.browser);
-            var name = browser.name.toLowerCase().replace(' ', '').replace('-','');
-            return {
-                browser: name,
-                visitors: item.count,
-                timing: self._getStubTimings(name, browser.version),
-                temperature: self._getStubTemperature(name, browser.version),
-                browserVersion: browser.version
-            };
-        });
+        var activeTab = 421139;
+
+        var mappedData = this._mapData(data, activeTab);
 
         if (!this._intializedData) {
-            this.barContainer.html(this.templates.statBars(browserData));
+            this.barContainer.html(this.templates.statBars(mappedData));
         }
         else {
-            this._updateData(browserData);
+            this._updateData(mappedData);
         }
 
         if(data && data.length > 0) {
@@ -58,73 +48,71 @@ DJ.UI.BrowserStats = DJ.UI.Component.extend({
         }
     },
     
+    _mapData: function (data, groupToReturn) {
+        if (!data || !data.length)
+            return [];
+        
+        var self = this;
+        
+        var browserStatsByPages = _.groupBy(data, function (item) {
+            return item.page_id;
+        });
+
+        var workingSet = browserStatsByPages[groupToReturn];
+
+        var pageLoadTimings = 0;
+
+        _.each(workingSet, function (item) {
+            pageLoadTimings += parseInt(item.Avg, 10);
+        });
+
+        var avgPageLoadAcrossBrowsers = pageLoadTimings / workingSet.length;
+        var greenZone = 5000; //avgPageLoadAcrossBrowsers * 0.3; // 30%
+        var neutralZone = 7000; //avgPageLoadAcrossBrowsers * 0.7; // 30% < x < 70%
+        
+        
+        var browserData = _.map(workingSet, function (item) {
+            var browser = self._parseBrowserInfo(item.Browser),
+                avg = parseInt(item.Avg, 10),
+                zone = avg < greenZone ? 'cool' : (avg <= neutralZone ? 'neutral' : 'hot');
+            return {
+                browser: browser.name.toLowerCase(),
+                visitors: item.Count,
+                timing: Math.round(avg / 1000),
+                percent: Math.min(avg / avgPageLoadAcrossBrowsers * 100, 100),
+                temperature: zone,
+                browserVersion: browser.version
+            };
+        });
+
+        return browserData;
+
+    },
+    
     _updateData: function (data) {
         var trafficBars = this.barContainer.find('.dj-trafficBar');
         for (var i = 0, len = data.length; i < len; i++) {
             var stat = data[i],
                 trafficBar = $(trafficBars[i]),
-                broswerLogo = stat.browser === 'internetexplorer' ? 'ie' + stat.browserVersion : stat.browser,
-                version = (stat.browser === 'internetexplorer' || (stat.browser === 'firefox' && stat.browserVersion < 4)) ? stat.browserVersion : '';
+                version = (stat.browser === 'msie' || (stat.browser === 'firefox' && stat.browserVersion < 4)) ? stat.browserVersion : '';
             trafficBar.find('.visitors').counter(stat.visitors);
             trafficBar.find('.bar').removeClass().addClass('bar').addClass(stat.temperature);
-            trafficBar.find('.timing').text(stat.timing + 'ms');
-            trafficBar.find('.gauge').animate({width: Math.round((stat.timing * 100) / 210) + '%'}, 600);
-            trafficBar.find('.browser i').removeClass().addClass('dj-logo-' + broswerLogo);
+            trafficBar.find('.timing').text(stat.timing + 's');
+            trafficBar.find('.gauge').animate({width: stat.percent + '%'}, 600);
+            trafficBar.find('.browser i').removeClass().addClass('dj-logo-' + stat.browser + version);
             trafficBar.find('.browserVersion').text(version);
         }
     },
 
-        /* TODO: dummy data related functions. review their worthiness after requirements are finalized */
-    _parseBrowserInfo: function (info) {
-        var items = info.split(' ');
-        var name = items[0];
-        var version = items.length > 1 ? items[1] : '';
         
-        if (items.length === 3) {
-            name = items[0] + ' ' + items[1];
-            version = items[2];
-        }
-
+    _parseBrowserInfo: function (info) {
+        // browser string looks like msie9, chrome, firefox3.6
+        var items = info.split(/(\d+(\.\d)?)$/);
+        
         return {
-            name: name,
-            version: version
+            name: items[0],
+            version: items.length > 0 ? items[1] : ''
         };
-    },
-
-    _getStubTimings: function (browser, version) {
-        var min = 10, max = 10;
-        switch (browser.toLowerCase()) {
-            case 'chrome':
-            case 'safari':
-                max = 30;
-                break;
-            case 'firefox':
-            case 'opera':
-                max = 70;
-                break;
-            case 'internetexplorer':
-                max = version < 9 ? 150 : 80;
-                break;
-            default:
-                max = 100;
-        }
-
-        return Math.floor(Math.random() * max) + min;
-    },
-
-    _getStubTemperature: function (browser, version) {
-        switch (browser.toLowerCase()) {
-            case 'chrome':
-            case 'safari':
-                return 'cool';
-            case 'firefox':
-            case 'opera':
-                return 'neutral';
-            case 'internetexplorer':
-                return version < 9 ? 'hot' : 'neutral';
-            default:
-                return 'neutral';
-        }
     }
 });
 
