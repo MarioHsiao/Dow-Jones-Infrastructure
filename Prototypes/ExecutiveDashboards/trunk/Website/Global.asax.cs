31561b823479557b82d740ad3c099a60015547be
chadwickj <chadwickj@djnamericas.tfs.local>
2012-09-24 19:45:00 +0000

    Cancelling polling tasks on app stop
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C94507

--- Prototypes/ExecutiveDashboards/trunk/Website/Global.asax.cs
+++ Prototypes/ExecutiveDashboards/trunk/Website/Global.asax.cs
@@ -1,16 +1,17 @@
 ﻿using System;
+using DowJones.Dash.DataSources;
 
 namespace DowJones.Dash.Website
 {
     public partial class MvcApplication
     {
-/*
-        public static string Version
-        {
-            get { return _version.Value; }
-        }
-*/
-        public static Lazy<string> Version = 
+        public static Lazy<string> Version =
             new Lazy<string>(typeof(MvcApplication).Assembly.GetName().Version.ToString);
+
+        protected override void OnApplicationStopped()
+        {
+            // Clean up outstanding polling tasks
+            PollingDataSource.GlobalCancellationTokenSource.Cancel(false);
+        }
     }
 }
