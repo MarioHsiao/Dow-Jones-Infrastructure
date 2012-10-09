bd04b6e19c2bef7e5cf7ae8417192bc0659d0e51
Panda, Hrusikesh <Hrusikesh.Panda@dowjones.com>
2012-10-09 19:03:23 +0000

    World map popup
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C95474

--- Prototypes/ExecutiveDashboards/trunk/DowJones.Dash.Components/StatsMap/StatsMap.js
+++ Prototypes/ExecutiveDashboards/trunk/DowJones.Dash.Components/StatsMap/StatsMap.js
@@ -8,22 +8,21 @@ DJ.UI.StatsMap = DJ.UI.Component.extend({
         dataEvent: 'data.PageLoadDetailsBySubCountryforCountry',
         map: 'us'
     },
-    
+
     selectors: {
         pillContainer: '.pillContainer',
         pill: '.dj-pills > li',
         noDataContainer: '.noData',
         contentContainer: '.content',
-        worldMapContainer: '.worldMapContainer',
-        worldMapModalHeader: '.modal-header h3'
+        worldViewToggler: '.worldViewToggler'
     },
 
     stateCodes: {
         us: { 1871: "Alabama", 1885: "Alaska", 1853: "Arizona", 1879: "Arkansas", 1854: "California", 1884: "Colorado", 1880: "Connecticut", 1881: "Delaware", 1858: "District of Columbia", 1856: "Florida", 1859: "Georgia", 1882: "Hawaii", 1869: "Idaho", 1855: "Illinois", 1870: "Indiana", 1886: "Iowa", 1851: "Kansas", 1900: "Kentucky", 1897: "Louisiana", 1802: "Maine", 1872: "Maryland", 1866: "Massachusetts", 1857: "Michigan", 1868: "Minnesota", 1883: "Mississippi", 1852: "Missouri", 1899: "Montana", 1895: "Nebraska", 1898: "Nevada", 1893: "New Hampshire", 1861: "New Jersey", 1863: "New Mexico", 1865: "New York", 1860: "North Carolina", 1887: "North Dakota", 1877: "Ohio", 1874: "Oklahoma", 1878: "Oregon", 1873: "Pennsylvania", 1890: "Rhode Island", 1896: "South Carolina", 1889: "South Dakota", 1888: "Tennessee", 1862: "Texas", 1867: "Utah", 1892: "Vermont", 1876: "Virginia", 1864: "Washington", 1891: "West Virginia", 1894: "Wisconsin", 1902: "Wyoming" },
         de: { 540: "Baden-Wurttemberg", 548: "Bavaria", 547: "Berlin", 542: "Brandenburg", 549: "Bremen", 550: "Hamburg", 551: "Hesse", 543: "Mecklenburg-Vorpommern", 552: "Lower Saxony", 553: "North Rhine-Westphalia", 554: "Rhineland-Palatinate", 555: "Saarland", 544: "Saxony", 545: "Saxony-Anhalt", 541: "Schleswig-Holstein", 546: "Thuringia" },
-        world: { 6:"Afghanistan", 9:"Albania", 62:"Algeria", 15:"American Samoa", 4:"Andorra", 12:"Angola", 7:"Antigua and Barbuda", 14:"Argentina", 10:"Armenia", 18:"Aruba", 17:"Australia", 16:"Austria", 19:"Azerbaijan", 33:"Bahamas, The", 26:"Bahrain", 22:"Bangladesh", 21:"Barbados", 37:"Belarus", 23:"Belgium", 38:"Belize", 28:"Benin", 34:"Bhutan", 31:"Bolivia", 20:"Bosnia and Herzegovina", 36:"Botswana", 32:"Brazil", 30:"Brunei Darussalam", 25:"Bulgaria", 24:"Burkina Faso", 27:"Burundi", 114:"Cambodia", 48:"Cameroon", 39:"Canada", 121:"Cayman Islands", 42:"Central African Republic", 206:"Chad", 47:"Chile", 49:"China", 50:"Colombia", 43:"Congo", 51:"Costa Rica", 45:"Cote d'Ivoire", 97:"Croatia", 52:"Cuba", 55:"Cyprus", 56:"Czech Republic", 59:"Denmark", 58:"Djibouti", 60:"Dominica", 61:"Dominican Republic", 63:"Ecuador", 65:"Egypt, Arab Rep.", 202:"El Salvador", 87:"Equatorial Guinea", 67:"Eritrea", 64:"Estonia", 69:"Ethiopia", 71:"Fiji", 70:"Finland", 75:"France", 76:"Gabon", 84:"Gambia, The", 79:"Georgia", 57:"Germany", 81:"Ghana", 88:"Greece", 83:"Greenland", 78:"Grenada", 90:"Guatemala", 85:"Guinea", 93:"Guyana", 98:"Haiti", 96:"Honduras", 99:"Hungary", 107:"Iceland", 103:"India", 100:"Indonesia", 106:"Iran, Islamic Rep.", 105:"Iraq", 101:"Ireland", 102:"Israel", 108:"Italy", 109:"Jamaica", 111:"Japan", 110:"Jordan", 122:"Kazakhstan", 112:"Kenya", 118:"Korea, Dem. Rep.", 119:"Korea, Rep.", 120:"Kuwait", 113:"Kyrgyz Republic", 123:"Lao PDR", 132:"Latvia", 124:"Lebanon", 129:"Lesotho", 128:"Liberia", 133:"Libya", 130:"Lithuania", 131:"Luxembourg", 139:"Macedonia, FYR", 137:"Madagascar", 151:"Malawi", 153:"Malaysia", 140:"Mali", 148:"Malta", 146:"Mauritania", 152:"Mexico", 136:"Moldova", 135:"Monaco", 142:"Mongolia", 147:"Montenegro", 134:"Morocco", 154:"Mozambique", 141:"Myanmar", 155:"Namibia", 163:"Nepal", 161:"Netherlands", 156:"New Caledonia", 166:"New Zealand", 160:"Nicaragua", 157:"Niger", 159:"Nigeria", 162:"Norway", 167:"Oman", 173:"Pakistan", 168:"Panama", 171:"Papua New Guinea", 180:"Paraguay", 169:"Peru", 172:"Philippines", 174:"Poland", 178:"Portugal", 176:"Puerto Rico", 181:"Qatar", 183:"Romania", 184:"Russian Federation", 185:"Rwanda", 186:"Saudi Arabia", 198:"Senegal", 196:"Sierra Leone", 191:"Singapore", 195:"Slovak Republic", 193:"Slovenia", 187:"Solomon Islands", 199:"Somalia", 238:"South Africa", 68:"Spain", 127:"Sri Lanka", 189:"Sudan", 200:"Suriname", 204:"Swaziland", 190:"Sweden", 44:"Switzerland", 203:"Syria Arab Republic", 210:"Tajikistan", 219:"Tanzania", 209:"Thailand", 208:"Togo", 214:"Tonga", 216:"Trinidad and Tobago", 213:"Tunisia", 215:"Turkey", 212:"Turkmenistan", 221:"Uganda", 220:"Ukraine", 5:"United Arab Emirates", 77:"United Kingdom", 223:"United States", 224:"Uruguay", 225:"Uzbekistan", 232:"Vanuatu", 228:"Venezuela, RB", 231:"Vietnam", 235:"Yemen, Rep.", 3001:"Zaire", 239:"Zambia", 240:"Zimbabwe"}
+        world: { 6: "Afghanistan", 9: "Albania", 62: "Algeria", 15: "American Samoa", 4: "Andorra", 12: "Angola", 7: "Antigua and Barbuda", 14: "Argentina", 10: "Armenia", 18: "Aruba", 17: "Australia", 16: "Austria", 19: "Azerbaijan", 33: "Bahamas, The", 26: "Bahrain", 22: "Bangladesh", 21: "Barbados", 37: "Belarus", 23: "Belgium", 38: "Belize", 28: "Benin", 34: "Bhutan", 31: "Bolivia", 20: "Bosnia and Herzegovina", 36: "Botswana", 32: "Brazil", 30: "Brunei Darussalam", 25: "Bulgaria", 24: "Burkina Faso", 27: "Burundi", 114: "Cambodia", 48: "Cameroon", 39: "Canada", 121: "Cayman Islands", 42: "Central African Republic", 206: "Chad", 47: "Chile", 49: "China", 50: "Colombia", 43: "Congo", 51: "Costa Rica", 45: "Cote d'Ivoire", 97: "Croatia", 52: "Cuba", 55: "Cyprus", 56: "Czech Republic", 59: "Denmark", 58: "Djibouti", 60: "Dominica", 61: "Dominican Republic", 63: "Ecuador", 65: "Egypt, Arab Rep.", 202: "El Salvador", 87: "Equatorial Guinea", 67: "Eritrea", 64: "Estonia", 69: "Ethiopia", 71: "Fiji", 70: "Finland", 75: "France", 76: "Gabon", 84: "Gambia, The", 79: "Georgia", 57: "Germany", 81: "Ghana", 88: "Greece", 83: "Greenland", 78: "Grenada", 90: "Guatemala", 85: "Guinea", 93: "Guyana", 98: "Haiti", 96: "Honduras", 99: "Hungary", 107: "Iceland", 103: "India", 100: "Indonesia", 106: "Iran, Islamic Rep.", 105: "Iraq", 101: "Ireland", 102: "Israel", 108: "Italy", 109: "Jamaica", 111: "Japan", 110: "Jordan", 122: "Kazakhstan", 112: "Kenya", 118: "Korea, Dem. Rep.", 119: "Korea, Rep.", 120: "Kuwait", 113: "Kyrgyz Republic", 123: "Lao PDR", 132: "Latvia", 124: "Lebanon", 129: "Lesotho", 128: "Liberia", 133: "Libya", 130: "Lithuania", 131: "Luxembourg", 139: "Macedonia, FYR", 137: "Madagascar", 151: "Malawi", 153: "Malaysia", 140: "Mali", 148: "Malta", 146: "Mauritania", 152: "Mexico", 136: "Moldova", 135: "Monaco", 142: "Mongolia", 147: "Montenegro", 134: "Morocco", 154: "Mozambique", 141: "Myanmar", 155: "Namibia", 163: "Nepal", 161: "Netherlands", 156: "New Caledonia", 166: "New Zealand", 160: "Nicaragua", 157: "Niger", 159: "Nigeria", 162: "Norway", 167: "Oman", 173: "Pakistan", 168: "Panama", 171: "Papua New Guinea", 180: "Paraguay", 169: "Peru", 172: "Philippines", 174: "Poland", 178: "Portugal", 176: "Puerto Rico", 181: "Qatar", 183: "Romania", 184: "Russian Federation", 185: "Rwanda", 186: "Saudi Arabia", 198: "Senegal", 196: "Sierra Leone", 191: "Singapore", 195: "Slovak Republic", 193: "Slovenia", 187: "Solomon Islands", 199: "Somalia", 238: "South Africa", 68: "Spain", 127: "Sri Lanka", 189: "Sudan", 200: "Suriname", 204: "Swaziland", 190: "Sweden", 44: "Switzerland", 203: "Syria Arab Republic", 210: "Tajikistan", 219: "Tanzania", 209: "Thailand", 208: "Togo", 214: "Tonga", 216: "Trinidad and Tobago", 213: "Tunisia", 215: "Turkey", 212: "Turkmenistan", 221: "Uganda", 220: "Ukraine", 5: "United Arab Emirates", 77: "United Kingdom", 223: "United States", 224: "Uruguay", 225: "Uzbekistan", 232: "Vanuatu", 228: "Venezuela, RB", 231: "Vietnam", 235: "Yemen, Rep.", 3001: "Zaire", 239: "Zambia", 240: "Zimbabwe" }
     },
-    
+
     dataLabelOptions: {
         Alaska: { y: -10 },
         California: { x: -10, y: 20 },
@@ -103,8 +102,6 @@ DJ.UI.StatsMap = DJ.UI.Component.extend({
         this._initializeChartProps();
         this._initializeChart();
 
-        //this._initWorldMap();
-        
         this._showContent();
         if (this.data) {
             this.setData(this.data);
@@ -122,16 +119,17 @@ DJ.UI.StatsMap = DJ.UI.Component.extend({
     _initializeElements: function (ctx) {
         this.$element.html(this.templates.container());
         this.pillContainer = ctx.find(this.selectors.pillContainer);
-        this.worldMapContainer = ctx.find(this.selectors.worldMapContainer);
 
-        var moduleTitle = ctx.closest('.dj_module').find('.dj_module-title').text();
-        ctx.find(this.selectors.worldMapModalHeader).text(moduleTitle);
+        if (this.options.map === 'world')
+            ctx.find('.worldViewToggler').hide();
     },
 
     _initializeEventHandlers: function () {
         $dj.subscribe(this.options.dataEvent, this._delegates.setData);
-        $dj.subscribe('comm.domain.changed', this._delegates.domainChanged);
-        
+
+        if (this.options.map !== 'world')
+            $dj.subscribe('comm.domain.changed', this._delegates.domainChanged);
+
         var self = this;
         this.$element.on('click', this.selectors.pill, function () {
             var el = $(this);
@@ -140,44 +138,38 @@ DJ.UI.StatsMap = DJ.UI.Component.extend({
             if (self.chartGroupsData)
                 self.chart.series[0].setData(self._getChartData(self.chartGroupsData[self.activePillId]));
         });
+
+        this.$element.on('click', this.selectors.worldViewToggler, function () {
+            self.publish('worldView.dj.StatsMap');
+        });
     },
-    
-    _initializeChartProps: function (map){
+
+    _initializeChartProps: function (map) {
         map = map || this.options.map;
         this.map = Highcharts.Maps[map];
         this.territories = this.map.territories;
         this.paths = this.map.paths;
-        this.stateCodes.current = this.stateCodes[map];
+        this.currentStateCodes = this.stateCodes[map];
 
         this.activePillId = null;
     },
 
-    _initWorldMap: function() {
-        DJ.add("StatsMap", {
-            container: this.worldMapContainer,
-            options: {
-                dataEvent: 'data.PageLoadDetailsByRegion',
-                map: 'world'
-            }
-        });
-    },
-    
     _domainChanged: function (data) {
         if (!data)
             return;
 
         this.domain = data;
-        
+
         this._initializeChartProps(data.map);
         this._initializeChart();
     },
-       
+
     _initializeChart: function () {
         this._initializingChart = true;
-        
+
         // highcharts will wipe out series object, hence initialize this
         this.mapConfig.series = this.mapConfig.series || [{ type: 'map', data: [] }];
-        
+
         for (var i = 0; i < this.territories.length; i++) {
             var key = this.territories[i];
 
@@ -190,20 +182,20 @@ DJ.UI.StatsMap = DJ.UI.Component.extend({
 
         // blank out previous maps
         this.$element.find('.mapContainer').html('');
-        
+
         this.mapConfig.chart.renderTo = this.$element.find('.mapContainer')[0];
-        
+
         if (this.chart) this.chart.destroy();
-        
+
         this.chart = new Highcharts.Map(this.mapConfig);
-        
+
         this._initializingChart = false;
     },
 
     setData: function (data) {
         // don't bother until chart is initialized
         if (this._initializingChart) return;
-        
+
         if (!data || !data.length) {
             this._showComingSoon();
             return;
@@ -231,7 +223,7 @@ DJ.UI.StatsMap = DJ.UI.Component.extend({
         for (var i = 0; i < pills.length; i++) {
             pills[i].active = this.activePillId === pills[i].id;
         }
-        
+
         this.pillContainer.html(this.templates.navPills(pills));
     },
 
@@ -240,14 +232,14 @@ DJ.UI.StatsMap = DJ.UI.Component.extend({
             return;
 
         var self = this;
-        
+
         // from a simple array, map it to an associative array
         // with the state abbreviation as the key. this makes subsequent lookups o(1) operation.
         var stateMap = {};
         _.each(workingSet, function (item) {
-            var state = self.stateCodes.current[item.subcountry_id];
+            var state = self.currentStateCodes[item.Id];
             stateMap[state] = {
-                name: item.subcountry_name,
+                name: item.Name,
                 avg: parseFloat((item.Avg / 1000).toFixed(2)),
                 min: parseFloat((item.Min / 1000).toFixed(2)),
                 max: parseFloat((item.Max / 1000).toFixed(2))
@@ -305,7 +297,7 @@ DJ.UI.StatsMap = DJ.UI.Component.extend({
         this.$element.find(this.selectors.contentContainer).show('fast');
         this.$element.find(this.selectors.noDataContainer).hide('fast');
     },
-    
+
     _getMarker: function (num) {
         if (num <= 5000)
             return '<%= WebResource("DowJones.Dash.Components.StatsMap.marker_rounded_yellow_green.png") %>';
