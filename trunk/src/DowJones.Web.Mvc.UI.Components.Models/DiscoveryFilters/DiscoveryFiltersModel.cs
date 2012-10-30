a9a68efe8b5d10a0767dd12956c73e0534944598
Gavara, Dilip <Dilip.Gavara@dowjones.com>
2012-10-30 22:00:15 +0000

    Added Discovery Filters Component
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C98067

---
+++ trunk/src/DowJones.Web.Mvc.UI.Components.Models/DiscoveryFilters/DiscoveryFiltersModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DowJones.Web.Mvc.UI.Components.DiscoveryGraph;
+
+namespace DowJones.Web.Mvc.UI.Components.DiscoveryFilters
+{
+    public class DiscoveryFiltersModel : ViewComponentModel
+    {
+        /// <summary>
+        /// Client Data
+        /// </summary>
+        [ClientData]
+        public Entities Data { get; set; }
+    }
+}
