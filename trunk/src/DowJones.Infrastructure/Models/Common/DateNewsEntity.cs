555032b2bad2e6838ee01910d5e37a241318defd
Hui, Ting <Ting.Hui@dowjones.com>
2012-11-02 16:04:55 +0000

    Entend the news entity to have the new classes to support additional properties for source family and date navigator.
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C98236

---
+++ trunk/src/DowJones.Infrastructure/Models/Common/DateNewsEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DowJones.Models.Common
+{
+    [DataContract(Name = "dateNewsEntity", Namespace = "")]
+    public class DateNewsEntity : NewsEntity
+    {
+        [DataMember(Name = "startDate")]
+        public DateTime StartDate { get; set; }
+
+        [DataMember(Name = "endDate")]
+        public DateTime EndDate { get; set; }
+    }
+}
