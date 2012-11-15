8328e60772a6f358fea0b6fd2d10f9fb57952c04
Panda, Hrusikesh <Hrusikesh.Panda@dowjones.com>
2012-11-15 22:10:59 +0000

    Show truncate title if available
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C99054

--- Prototypes/DowJones.Factiva.Currents/trunk/DowJones.Factiva.Currents.Components/CurrentsHeadline/CurrentsHeadlineComponent.cs
+++ Prototypes/DowJones.Factiva.Currents/trunk/DowJones.Factiva.Currents.Components/CurrentsHeadline/CurrentsHeadlineComponent.cs
@@ -23,7 +23,7 @@ namespace DowJones.Factiva.Currents.Components.CurrentsHeadline
     using CurrentsHeadline;
     using DowJones.Web.Mvc.Extensions;
     
-    // Last Generated Timestamp: 11/09/2012 03:32 PM
+    // Last Generated Timestamp: 11/15/2012 05:08 PM
     [DowJones.Web.ScriptResourceAttribute(null, ResourceName="DowJones.Factiva.Currents.Components.CurrentsHeadline.CurrentsHeadline.js", ResourceKind=DowJones.Web.ClientResourceKind.Script, DeclaringType=typeof(DowJones.Factiva.Currents.Components.CurrentsHeadline.CurrentsHeadlineComponent))]
     [System.CodeDom.Compiler.GeneratedCodeAttribute("RazorViewComponentClassGenerator", "1.0.0.27108")]
     public class CurrentsHeadlineComponent : DowJones.Web.Mvc.UI.ViewComponentBase<CurrentsHeadlineModel>
@@ -81,7 +81,7 @@ WriteLiteral("\">\r\n\t\t\t\t<div class=\"article-wrap\">\r\n\t\t\t\t\t<h4 class
 WriteLiteral("\" class=\"article-view-trigger\" target=\"_blank\">");
 
 
-                                                                                                Write(headline.Title);
+                                                                                                 Write(headline.TruncatedTitle ?? headline.Title);
 
 WriteLiteral("</a>\r\n\t\t\t\t\t</h4>\r\n");
 
