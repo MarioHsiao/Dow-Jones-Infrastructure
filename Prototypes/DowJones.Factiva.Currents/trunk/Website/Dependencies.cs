a18d153eb8207a73d705ec89f3039ccd7038585d
Panda, Hrusikesh <Hrusikesh.Panda@dowjones.com>
2012-12-04 18:25:34 +0000

    Disable caching when debuggin is on
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C100151

--- Prototypes/DowJones.Factiva.Currents/trunk/Website/Dependencies.cs
+++ Prototypes/DowJones.Factiva.Currents/trunk/Website/Dependencies.cs
@@ -1,4 +1,5 @@
-﻿using DowJones.Factiva.Currents.ServiceModels.PageService;
+﻿using System.Web;
+using DowJones.Factiva.Currents.ServiceModels.PageService;
 using DowJones.Factiva.Currents.Website.Contracts;
 using DowJones.Factiva.Currents.Website.Mocks;
 using DowJones.Factiva.Currents.Website.Providers;
@@ -21,23 +22,30 @@ namespace DowJones.Factiva.Currents.Website
 		protected override void OnLoad()
 		{
 			Bind<IControlData>().ToMethod(x => new ControlData()).InRequestScope();
-			Bind<IUserSession>().ToMethod(x => new UserSession { SessionId = "12345" }).InRequestScope();
+			Bind<IUserSession>().ToMethod(x => new UserSession {SessionId = "12345"}).InRequestScope();
 			Bind<IPreferences>().ToMethod(x => new Preferences.Preferences("en")).InRequestScope();
 			Bind<IPrinciple>().ToMethod(x => new EntitlementsPrinciple(new GetUserAuthorizationsResponse())).InRequestScope();
 			Bind<Product>().ToConstant(new GlobalProduct()).InSingletonScope();
 
 			Bind<JsonSerializerSettings>()
 				.ToConstant(new JsonSerializerSettings
-				{
-					NullValueHandling = NullValueHandling.Ignore,
-					ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-					ContractResolver = new CamelCasePropertyNamesContractResolver(),
-					Converters = new[] { new StringEnumConverter() },
-					TypeNameHandling = TypeNameHandling.Objects,
-				});
+					{
+						NullValueHandling = NullValueHandling.Ignore,
+						ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+						ContractResolver = new CamelCasePropertyNamesContractResolver(),
+						Converters = new[] {new StringEnumConverter()},
+						TypeNameHandling = TypeNameHandling.Objects,
+					});
 
-			Bind(typeof(ICacheProvider<>)).To(typeof(HttpContextCacheProvider<>)).InRequestScope();
-			Bind<IPageAssetProvider>().To<CachedPageAssetProvider>().InRequestScope();
+			if (HttpContext.Current.IsDebuggingEnabled)
+			{
+				Bind<IPageAssetProvider>().To<PageAssetProvider>().InRequestScope();
+			}
+			else
+			{
+				Bind(typeof(ICacheProvider<>)).To(typeof(HttpContextCacheProvider<>)).InRequestScope();
+				Bind<IPageAssetProvider>().To<CachedPageAssetProvider>().InRequestScope();
+			}
 
 			Bind<IScriptModuleTemplateManager>().To<MockScriptModuleTemplateManager>();
 
