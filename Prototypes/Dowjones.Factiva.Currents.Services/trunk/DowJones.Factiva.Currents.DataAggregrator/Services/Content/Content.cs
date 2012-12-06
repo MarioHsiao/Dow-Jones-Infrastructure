20f558749348c18c7318532e3a6f71667bdb6915
Goyal, Ruchi <Ruchi.Goyal@dowjones.com>
2012-12-06 16:23:01 +0000

    Adding Content Service
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C100369

---
+++ Prototypes/Dowjones.Factiva.Currents.Services/trunk/DowJones.Factiva.Currents.DataAggregrator/Services/Content/Content.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml;
+using DowJones.Factiva.Currents.Common;
+using DowJones.Factiva.Currents.Common.ExceptionHandling;
+using DowJones.Factiva.Currents.Common.Logging;
+using CurrentUtilities = DowJones.Factiva.Currents.Common.Utilities;
+using Newtonsoft.Json;
+using DowJones.Factiva.Currents.Common.Utilities;
+
+namespace DowJones.Factiva.Currents.Aggregrator.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Content" in both code and config file together.
+    public class Content : IContent
+    {
+        public Stream GetHeadlines(string format, string searchContextRef)
+        {
+            string result = string.Empty;
+            MemoryStream stream = null;
+            try
+            {
+                result = Common.GetHeadlines(format, searchContextRef);
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message + "_" + ex.StackTrace;
+            }
+            finally
+            {
+                byte[] byteArray = Encoding.Default.GetBytes(result);
+                stream = new MemoryStream(byteArray);
+                CurrentUtilities.Web.SetResponseHeaders(EnumConverter<RequestFormat>.ConvertStringToEnum(format));
+            }
+            return stream;
+        }
+
+        public Stream GetHeadlinesByAccessionNumber(string accessionNumber, string format)
+        {
+            string result = string.Empty;
+            MemoryStream stream = null;
+            try
+            {
+                result = Common.GetHeadlinesByAccessionNumber(accessionNumber, format);
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message + "_" + ex.StackTrace;
+            }
+            finally
+            {
+                byte[] byteArray = Encoding.Default.GetBytes(result);
+                stream = new MemoryStream(byteArray);
+                CurrentUtilities.Web.SetResponseHeaders(EnumConverter<RequestFormat>.ConvertStringToEnum(format));
+            }
+            return stream;
+        }
+    }
+}
---
+++ Prototypes/Dowjones.Factiva.Currents.Services/trunk/DowJones.Factiva.Currents.DataAggregrator/Services/Content/IContent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DowJones.Factiva.Currents.Aggregrator.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IContent" in both code and config file together.
+    [ServiceContract(Name = "IContentService", Namespace = "")]
+    public interface IContent
+    {
+        [Description("Get Headlines")]
+        [WebGet(UriTemplate = "/headlines/{format}?searchContextRef={searchContextRef}", BodyStyle = WebMessageBodyStyle.Bare)]
+        [OperationContract(Name = "GetHeadlines")]
+        Stream GetHeadlines(string format, string searchContextRef);
+
+        [Description("Get Headlines By AccessionNumber")]
+        [WebGet(UriTemplate = "/headlines/an/{format}?an={accessionNumber}", BodyStyle = WebMessageBodyStyle.Bare)]
+        [OperationContract(Name = "GetHeadlinesByAccessionNumber")]
+        Stream GetHeadlinesByAccessionNumber(string accessionNumber, string format);
+    }
+}
