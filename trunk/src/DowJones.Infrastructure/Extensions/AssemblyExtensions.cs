1aab2c575b0e9b7086a7ab1233e139619c24a7b5
Panda, Hrusikesh <Hrusikesh.Panda@dowjones.com>
2012-12-07 19:50:17 +0000

    Add resourcename to webresource always (flow player breaks otherwise)
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C100512

--- trunk/src/DowJones.Infrastructure/Extensions/AssemblyExtensions.cs
+++ trunk/src/DowJones.Infrastructure/Extensions/AssemblyExtensions.cs
@@ -5,47 +5,49 @@ using System.Reflection;
 using System.Web;
 using System.Web.Handlers;
 using DowJones.Infrastructure;
+using System.Web.UI;
 
 namespace DowJones.Extensions
 {
-    public static class AssemblyExtensions
-    {
-        public static DateTime GetAssemblyTimestamp(this Assembly assembly)
-        {
-            return File.GetLastWriteTimeUtc(assembly.Location);
-        }
+	public static class AssemblyExtensions
+	{
+		public static DateTime GetAssemblyTimestamp(this Assembly assembly)
+		{
+			return File.GetLastWriteTimeUtc(assembly.Location);
+		}
 
-        private static readonly Lazy<MethodInfo> GetWebResourceUrlMethod = new Lazy<MethodInfo>(() =>
-        {
-            var method =
-                typeof(AssemblyResourceLoader).FindMembers(
-                    MemberTypes.Method,
-                    BindingFlags.Static | BindingFlags.NonPublic,
-                    (member, obj) => member.Name.ToString() == obj.ToString(),
-                    "GetWebResourceUrlInternal"
-                )
-                .OfType<MethodInfo>()
-                .Single(x => x.GetParameters().Length == 5);
+		private static readonly Lazy<MethodInfo> GetWebResourceUrlMethod = new Lazy<MethodInfo>(() =>
+		{
 
-            return method;
-        });
 
-        public static string GetWebResourceUrl(this Assembly targetAssembly, string resourceName)
-        {
-            Guard.IsNotNullOrEmpty(resourceName, "resourceName");
+			var method =
+				typeof(AssemblyResourceLoader).FindMembers(
+					MemberTypes.Method,
+					BindingFlags.Static | BindingFlags.NonPublic,
+					(member, obj) => member.Name.ToString() == obj.ToString(),
+					"GetWebResourceUrlInternal"
+				)
+				.OfType<MethodInfo>()
+				.Single(x => x.GetParameters().Length == 5);
 
-            var resourceUrl = (string)GetWebResourceUrlMethod.Value.Invoke(null, new object[] { targetAssembly, resourceName, false, false, null });
+			return method;
+		});
 
-            // If we're in debug mode, add the Resource Name to the URL
-            if (Properties.Settings.Default.IsDebugMode)
-            {
-                var encodedName = HttpUtility.UrlEncode(resourceName);
-                var queryParameter = string.Format("ResourceName={0}&", encodedName);
-                var queryStringStart = resourceUrl.IndexOf('?');
-                resourceUrl = resourceUrl.Insert(queryStringStart + 1, queryParameter);
-            }
+		public static string GetWebResourceUrl(this Assembly targetAssembly, string resourceName)
+		{
+			Guard.IsNotNullOrEmpty(resourceName, "resourceName");
 
-            return resourceUrl;
-        }
-    }
+			var resourceUrl = (string)GetWebResourceUrlMethod.Value.Invoke(null, new object[] { targetAssembly, resourceName, false, false, null });
+
+
+			// always add the resourcename since without it, flow player doesn't seem to work.
+			var encodedName = HttpUtility.UrlEncode(resourceName);
+			var queryParameter = string.Format("ResourceName={0}&", encodedName);
+			var queryStringStart = resourceUrl.IndexOf('?');
+			resourceUrl = resourceUrl.Insert(queryStringStart + 1, queryParameter);
+
+
+			return resourceUrl;
+		}
+	}
 }
