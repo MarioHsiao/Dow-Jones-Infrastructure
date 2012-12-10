0f82d71acb4117104076282bf6b050847a6b89bb
Panda, Hrusikesh <Hrusikesh.Panda@dowjones.com>
2012-12-10 20:31:24 +0000

    Refactoring
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C100610

--- Prototypes/DowJones.Factiva.Currents/trunk/Website/Controllers/ArticleController.cs
+++ Prototypes/DowJones.Factiva.Currents/trunk/Website/Controllers/ArticleController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Configuration;
-using System.Globalization;
 using System.Web.Mvc;
 using DowJones.Factiva.Currents.Website.Contracts;
 using DowJones.Factiva.Currents.Website.Models;
@@ -11,14 +9,10 @@ namespace DowJones.Factiva.Currents.Website.Controllers
 	public class ArticleController : Controller
 	{
         private readonly IContentProvider _contentProvider;
-        private readonly string _loginUrl;
-        private readonly string _signUpUrl;
 
         public ArticleController(IContentProvider contentProvider)
         {
             _contentProvider = contentProvider;
-            _loginUrl =  ConfigurationManager.AppSettings.Get("LoginUrl").Trim('/');
-            _signUpUrl = ConfigurationManager.AppSettings.Get("SignupUrl").Trim('/');
         }
 
 		[OutputCache(CacheProfile = "ArticleCache")]
@@ -30,14 +24,18 @@ namespace DowJones.Factiva.Currents.Website.Controllers
 			if (headline == null)
 				return View();
 
-
-
 			var model = new HeadlinePreview
 				{
 					Headline = headline,
 				};
 
             return View(model);
+		}
+
+		[OutputCache(CacheProfile = "ArticleCache")]
+		public ActionResult Video(int year, int month, int day, string name, string an)
+		{
+			return View();
 		}
 
 	}
