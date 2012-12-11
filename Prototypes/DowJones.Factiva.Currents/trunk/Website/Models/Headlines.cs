5ced440dd1b7e4d16ba35604c0db1bdf6e177a64
Goyal, Ruchi <Ruchi.Goyal@dowjones.com>
2012-12-11 19:59:17 +0000

    Fixing bugs
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C100692

--- Prototypes/DowJones.Factiva.Currents/trunk/Website/Models/Headlines.cs
+++ Prototypes/DowJones.Factiva.Currents/trunk/Website/Models/Headlines.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using DowJones.Ajax.PortalHeadlineList;
+using DowJones.Extensions;
 using DowJones.Factiva.Currents.Components.CurrentsHeadline;
 
 namespace DowJones.Factiva.Currents.Website.Models
@@ -7,6 +8,14 @@ namespace DowJones.Factiva.Currents.Website.Models
 	public class HeadlinePreview
 	{
 		public PortalHeadlineInfo Headline { get; set; }
+
+        public string FormattedDateTime(PortalHeadlineInfo headline)
+        {
+            if (headline.PublicationTimeDescriptor.HasValue())
+                return headline.PublicationDateTime.ToLocalTime().ToString("HH:mm") + "," + headline.PublicationDateDescriptor;
+            else
+                return headline.PublicationDateTimeDescriptor;
+        }
 	}
 
 	// TODO: figure out a better name
