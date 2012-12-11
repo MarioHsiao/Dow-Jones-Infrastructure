68348e073c2b4958c1743f15c589c0e5baef354b
Singh, Alka <Alka.Singh@dowjones.com>
2012-12-11 16:49:05 +0000

    Added new object for Simple Alert News Filters to handle exclude(Not) filter items.
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C100669

--- trunk/src/DowJones.Infrastructure/Alert/SimpleAlertRequest.cs
+++ trunk/src/DowJones.Infrastructure/Alert/SimpleAlertRequest.cs
@@ -3,6 +3,8 @@ using DowJones.Managers.Search.CodedNewsQueries;
 using DowJones.Utilities.Search.Core;
 using Factiva.Gateway.Messages.Preferences.V1_0;
 using Factiva.Gateway.Messages.Track.V1_0;
+using Newtonsoft.Json;
+using FilterItem = DowJones.Utilities.Search.Core.FilterItem;
 
 namespace DowJones.Infrastructure.Alert
 {
@@ -30,5 +32,32 @@ namespace DowJones.Infrastructure.Alert
     {
         public SearchSourceGroupPreferenceItem SourceList { get; set; }
         public List<Source> Source { get; set; }
+    }
+
+    public class AlertNewsFilters
+    {
+        [JsonProperty("author")]
+        public FilterList<FilterItem> Author { get; set; }
+
+        [JsonProperty("executive")]
+        public FilterList<FilterItem> Executive { get; set; }
+
+        [JsonProperty("industry")]
+        public FilterList<FilterItem> Industry { get; set; }
+
+        [JsonProperty("newssubject")]
+        public FilterList<FilterItem> Subject { get; set; }
+
+        [JsonProperty("region")]
+        public FilterList<FilterItem> Region { get; set; }
+
+        [JsonProperty("company")]
+        public FilterList<FilterItem> Company { get; set; }
+
+        [JsonProperty("companyoccur")]
+        public FilterList<FilterItem> CompanyOccur { get; set; }
+
+        [JsonProperty("source")]
+        public FilterList<FilterItem> Source { get; set; }
     }
 }
