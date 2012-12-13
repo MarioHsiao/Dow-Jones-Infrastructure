6ecd6b97bff6f76598ff421fb0033a399ccc291e
Panda, Hrusikesh <Hrusikesh.Panda@dowjones.com>
2012-12-13 16:10:10 +0000

    Working draft cut for popout.
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C100860

--- Prototypes/ExecutiveDashboards/trunk/Website/App_Start/BundlesConfig.cs
+++ Prototypes/ExecutiveDashboards/trunk/Website/App_Start/BundlesConfig.cs
@@ -3,19 +3,20 @@ using DowJones.Infrastructure;
 
 namespace DowJones.Dash.Website.App_Start
 {
-    public class BundlesConfigurationTask : IBootstrapperTask
-    {
-        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+	public class BundlesConfigurationTask : IBootstrapperTask
+	{
+		// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
 
-        public void Execute()
-        {
-            RegisterBundles(BundleTable.Bundles);
-        }
+		public void Execute()
+		{
+			RegisterBundles(BundleTable.Bundles);
+		}
 
-        public static void RegisterBundles(BundleCollection bundles)
-        {
-	        bundles.Add(new StyleBundle("~/css/core")
-		                    .Include(
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			
+			bundles.Add(new StyleBundle("~/css/core")
+							.Include(
 								"~/Content/bootstrap.css",
 								"~/Content/bootstrap-responsive.css",
 								"~/Content/layout.css",
@@ -24,21 +25,25 @@ namespace DowJones.Dash.Website.App_Start
 			bundles.Add(new StyleBundle("~/css/dashboard")
 							.Include(
 								"~/Content/dashboard.css"
-								,"~/Content/statsMap.css"
-								,"~/Content/platformStats.css"
-								,"~/Content/gallery.css"
-								,"~/Content/browserShare.css"
-								,"~/Content/concurrentVisits.css"
-								,"~/Content/perfGauge.css"
-								,"~/Content/topPages.css"
-								,"~/Content/pageTimings.css"
+								, "~/Content/statsMap.css"
+								, "~/Content/platformStats.css"
+								, "~/Content/gallery.css"
+								, "~/Content/browserShare.css"
+								, "~/Content/concurrentVisits.css"
+								, "~/Content/perfGauge.css"
+								, "~/Content/topPages.css"
+								, "~/Content/pageTimings.css"
 						));
 
-            bundles.Add(new ScriptBundle("~/bundles/core").Include(
-                "~/Scripts/bootstrap.*",
-                "~/Scripts/jquery.signalR-{version}.js"
-            ));
+			bundles.Add(new StyleBundle("~/css/popup")
+							.Include(
+								"~/Content/popup.css"));
 
-        }
-    }
+			bundles.Add(new ScriptBundle("~/bundles/core").Include(
+				"~/Scripts/bootstrap.*",
+				"~/Scripts/jquery.signalR-{version}.js"
+			));
+
+		}
+	}
 }
