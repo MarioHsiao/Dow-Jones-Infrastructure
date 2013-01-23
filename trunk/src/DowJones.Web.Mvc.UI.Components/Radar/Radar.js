27c876a51f29fd1f95d4aeaf3208e169ff396b23
Sangoli, Shankar <Shankar.Sangoli@dowjones.com>
2013-01-23 18:33:01 +0000

    Bug # 41050 fixed.
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C103087

--- trunk/src/DowJones.Web.Mvc.UI.Components/Radar/Radar.js
+++ trunk/src/DowJones.Web.Mvc.UI.Components/Radar/Radar.js
@@ -12,81 +12,81 @@
 * Radar
 */
 
-    DJ.UI.Radar = DJ.UI.Component.extend({
+DJ.UI.Radar = DJ.UI.Component.extend({
 
-        /*
-        * Properties
-        */
-        events: {
-            nodeClick: "nodeClick.dj.Radar"
-        },
+    /*
+    * Properties
+    */
+    events: {
+        nodeClick: "nodeClick.dj.Radar"
+    },
 
-        // Default options
-        defaults: {
-            debug: false,
-            cssClass: 'dj_Radar',
-            pageSize: 6
-        },
-
-
-        /*
-        * Initialization (constructor)
-        */
-        init: function (element, meta) {
-            var $meta = $.extend({ name: "Radar" }, meta);
-
-            // Call the base constructor
-            this._super(element, $meta);
-        },
+    // Default options
+    defaults: {
+        debug: false,
+        cssClass: 'dj_Radar',
+        pageSize: 6
+    },
 
 
-        /*
-        * Public methods
-        */
+    /*
+    * Initialization (constructor)
+    */
+    init: function (element, meta) {
+        var $meta = $.extend({ name: "Radar" }, meta);
 
-        setData: function (radarData) {
-            this.data = radarData;
-            this._renderRadar();
-        },
+        // Call the base constructor
+        this._super(element, $meta);
+    },
 
-        /*
-        * Private methods
-        */
-        _renderRadar: function () {
-            var self = this,
+
+    /*
+    * Public methods
+    */
+
+    setData: function (radarData) {
+        this.data = this._validateAndCorrectData(radarData);
+        this._renderRadar();
+    },
+
+    /*
+    * Private methods
+    */
+    _renderRadar: function () {
+        var self = this,
                 el = $(self.element),
                 o = self.options;
 
-            el.empty().html("");
-            var $moduleFooter = $('.module-footer', el.parents('.dj_module-core')).empty();
-            
-            if (self.data) {
-                var now = new Date();
-                // radar
-                var $radarContainer = $("<div class='radar-container'></div>").appendTo(el);
-                // 14 and 194 are right and left margins of radar-content, defined in css
-                var availableWidth = $radarContainer.width() - 14 - 194;
-                var pagesCount = Math.ceil(self.data.parentNewsEntities.length / o.pageSize);
-                //Ron: removing the dynamic calculation of the image width, IE7 calculates availableWidth as -208 due to $radarContainer.width() not having a Data 
-                //var imageWidth = Math.floor(availableWidth / o.pageSize);
-                var imageWidth = 128;
+        el.empty().html("");
+        var $moduleFooter = $('.module-footer', el.parents('.dj_module-core')).empty();
 
-                $radarContainer.append(this.templates.success({
-                    tokens: self.tokens,
-                    data: self.data,
-                    pageSize: o.pageSize,
-                    pagesCount: pagesCount,
-                    radarNodeTotalImageUrl: o.totalImageUrl,
-                    radarNodeValueImageUrl: o.valueImageUrl,
-                    //imgWidth: Math.floor(availableWidth / o.pageSize)
-                    imgWidth: imageWidth
-                }));
+        if (self.data) {
+            var now = new Date();
+            // radar
+            var $radarContainer = $("<div class='radar-container'></div>").appendTo(el);
+            // 14 and 194 are right and left margins of radar-content, defined in css
+            var availableWidth = $radarContainer.width() - 14 - 194;
+            var pagesCount = Math.ceil(self.data.parentNewsEntities.length / o.pageSize);
+            //Ron: removing the dynamic calculation of the image width, IE7 calculates availableWidth as -208 due to $radarContainer.width() not having a Data 
+            //var imageWidth = Math.floor(availableWidth / o.pageSize);
+            var imageWidth = 128;
 
-                $.each(el.find(".radar-box .radar-node"), function (i, v) {
-                    var $radarNode = $(v),
+            $radarContainer.append(this.templates.success({
+                tokens: self.tokens,
+                data: self.data,
+                pageSize: o.pageSize,
+                pagesCount: pagesCount,
+                radarNodeTotalImageUrl: o.totalImageUrl,
+                radarNodeValueImageUrl: o.valueImageUrl,
+                //imgWidth: Math.floor(availableWidth / o.pageSize)
+                imgWidth: imageWidth
+            }));
+
+            $.each(el.find(".radar-box .radar-node"), function (i, v) {
+                var $radarNode = $(v),
                         x = $radarNode.attr("x"),
                         y = $radarNode.attr("y");
-                    $radarNode
+                $radarNode
                         .unbind()
                         .bind('click', function (event) {
                             self.currentNodeClicked = this;
@@ -112,54 +112,54 @@
                             self.clearTrailNodes();
                             self.getInTrailNodes(this).removeClass('radar-node-intrail');
                         });
+            });
+
+            //pagination using jQuery cycle plugin                
+            if (pagesCount > 1) {
+                var $radarContentPages = el.find('.radar-content-pages');
+
+                //pager
+                //updated by Ron to center the pager
+                //TODO: this creates a dependancy of the module markup thus preventing the component pager to live anywhere besides a module
+
+                var $pager = $("<div class='module-pager'></div>").appendTo($moduleFooter);
+
+                //kill any previous cycle instances and inline styles
+                $radarContentPages.cycle('destroy').removeAttr('style').children().removeAttr('style').not(':first').hide();
+
+                el.doTimeout(250, function () {
+                    $radarContentPages.cycle({
+                        timeout: 0,
+                        fx: 'scrollHorz',
+                        pager: $pager,
+                        activePagerClass: 'pager-active',
+                        pagerAnchorBuilder: function (idx, slide) {
+                            return '<a class="pager-link pager-' + (idx + 1) + '"></a>';
+                        }
+                    });
                 });
 
-                //pagination using jQuery cycle plugin                
-                if (pagesCount > 1) {
-                    var $radarContentPages = el.find('.radar-content-pages');
-
-                    //pager
-                    //updated by Ron to center the pager
-                    //TODO: this creates a dependancy of the module markup thus preventing the component pager to live anywhere besides a module
-
-                    var $pager = $("<div class='module-pager'></div>").appendTo($moduleFooter);
-
-                    //kill any previous cycle instances and inline styles
-                    $radarContentPages.cycle('destroy').removeAttr('style').children().removeAttr('style').not(':first').hide();
-
-                    el.doTimeout(250, function () {
-                        $radarContentPages.cycle({
-                            timeout: 0,
-                            fx: 'scrollHorz',
-                            pager: $pager,
-                            activePagerClass: 'pager-active',
-                            pagerAnchorBuilder: function (idx, slide) {
-                                return '<a class="pager-link pager-' + (idx + 1) + '"></a>';
-                            }
-                        });
+                //No device check. If the device supports it will work or else will do nothing.  was-> if($.iDevices.iPad){
+                if (window.addEventListener) {
+                    $radarContentPages.touchwipe({
+                        wipeLeft: function () {
+                            $radarContentPages.cycle("next");
+                        },
+                        wipeRight: function () {
+                            $radarContentPages.cycle("prev");
+                        },
+                        preventDefaultEvents: false
                     });
-
-                    //No device check. If the device supports it will work or else will do nothing.  was-> if($.iDevices.iPad){
-                    if (window.addEventListener) {
-                        $radarContentPages.touchwipe({
-                            wipeLeft: function () {
-                                $radarContentPages.cycle("next");
-                            },
-                            wipeRight: function () {
-                                $radarContentPages.cycle("prev");
-                            },
-                            preventDefaultEvents: false
-                        });
-                    }
                 }
             }
-            else {
-                //render error template?
-            }
-        },
+        }
+        else {
+            //render error template?
+        }
+    },
 
-        getInTrailNodes: function (radarNode) {
-            var $radarNode = $(radarNode),
+    getInTrailNodes: function (radarNode) {
+        var $radarNode = $(radarNode),
                 $td = $radarNode.closest('td'),
                 $tr = $td.closest('tr'),
                 $page = $tr.closest('table'),
@@ -170,23 +170,48 @@
                 trail = $tds.slice(0, tdIdx + 1).get(),
                 $subjects = $(this.element).find('.radar-subject');
 
-            $trs.slice(0, trIdx + 1).each(function () {
-                var td = $(this).children().filter('td').get(tdIdx);
-                trail.push(td);
-            });
+        $trs.slice(0, trIdx + 1).each(function () {
+            var td = $(this).children().filter('td').get(tdIdx);
+            trail.push(td);
+        });
 
-            trail.push($subjects.get(trIdx));
+        trail.push($subjects.get(trIdx));
 
-            return $(trail).children().filter('.radar-node');
-        },
+        return $(trail).children().filter('.radar-node');
+    },
 
-        clearTrailNodes: function () {
-            if (this.currentNodeClicked) {
-                this.getInTrailNodes(this.currentNodeClicked).removeClass('radar-node-intrail');
-                this.currentNodeClicked = null;
+    clearTrailNodes: function () {
+        if (this.currentNodeClicked) {
+            this.getInTrailNodes(this.currentNodeClicked).removeClass('radar-node-intrail');
+            this.currentNodeClicked = null;
+        }
+    },
+
+    _getNewsEntityReference: function (data) {
+        var ref = data.parentNewsEntities[0].newsEntities;
+        for (var i = 1; i < data.parentNewsEntities.length; i++) {
+            if (ref.length < data.parentNewsEntities[i].newsEntities.length) {
+                ref = data.parentNewsEntities[i].newsEntities;
             }
         }
-    });
+        return ref;
+    },
+
+    _validateAndCorrectData: function (data) {
+        var newsEntityRef = this._getNewsEntityReference(data);
+        for (var i = 0; i < data.parentNewsEntities.length; i++) {
+            if (data.parentNewsEntities[i].newsEntities.length == newsEntityRef.length) {
+                continue;
+            }
+            for (j = 0; j < newsEntityRef.length; j++) {
+                if (!data.parentNewsEntities[i].newsEntities[j] || data.parentNewsEntities[i].newsEntities[j].code != newsEntityRef[j].code) {
+                    data.parentNewsEntities[i].newsEntities.splice(j, 0, $.extend(newsEntityRef[j], { currentTimeFrameNewsVolume: { displayText: { value: "0" }, value: 0} }));
+                }
+            }
+        }
+        return data;
+    }
+});
 
     // Declare this class as a jQuery plugin
     $.plugin('dj_Radar', DJ.UI.Radar);
