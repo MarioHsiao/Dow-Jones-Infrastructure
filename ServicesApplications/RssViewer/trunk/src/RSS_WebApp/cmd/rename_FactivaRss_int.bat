9dc9d18504c838be1d7824b8ff1ed9c901e425ce
Reddy, Praveen <Praveen.Reddy@dowjones.com>
2013-03-08 16:55:15 +0000

    Merge From Releases/4.10
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C105788

--- ServicesApplications/RssViewer/trunk/src/RSS_WebApp/cmd/rename_FactivaRss_int.bat
+++ ServicesApplications/RssViewer/trunk/src/RSS_WebApp/cmd/rename_FactivaRss_int.bat
@@ -1,5 +1,7 @@
 del E:\rss_web_site\config.xml
 del E:\rss_web_site\Web.config 
+del E:\rss_web_site\Log4Net.config 
 rename E:\rss_web_site\Web.config_int Web.config
 rename E:\rss_web_site\int_rss.not_on_this_server int_rss.hold
 rename E:\rss_web_site\config.xml_int config.xml
+rename E:\rss_web_site\Log4Net_integration.config Log4Net.config
