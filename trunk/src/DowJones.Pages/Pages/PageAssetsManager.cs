1c9754d3db88b698291baf8715b70ad93e24decc
Dacosta, David <David.Dacosta@dowjones.com>
2013-03-15 14:54:35 +0000

    Updated the public page signiture
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C106207

--- trunk/src/DowJones.Pages/Pages/PageAssetsManager.cs
+++ trunk/src/DowJones.Pages/Pages/PageAssetsManager.cs
@@ -815,6 +815,17 @@ namespace DowJones.Pages
         /// <param name="alertIds">
         /// The alert ids.
         /// </param>
+        public void MakeSaveSearchesPublic(IEnumerable<string> savedSearchIds)
+        {
+           //
+        }
+
+        /// <summary>
+        /// The make personal alerts public.
+        /// </summary>
+        /// <param name="alertIds">
+        /// The alert ids.
+        /// </param>
         public void MakePersonalAlertsPublic(IEnumerable<int> alertIds)
         {
             if (alertIds == null || !alertIds.Any())
@@ -849,14 +860,16 @@ namespace DowJones.Pages
         /// <param name="personalAlertIds">
         /// The personal alert ids.
         /// </param>
-        public void PublishPage(string pageId, IEnumerable<int> personalAlertIds)
+        public void PublishPage(string pageId, IEnumerable<int> personalAlertIds = null, IEnumerable<string> personalSavedSearches = null )
         {
             var page = GetPage(pageId, false, false);
 
             if (page != null && page.ModuleCollection != null)
             {
+                //  Use TPL to minimize this 
                 MakePageModulesPublic(page.ModuleCollection);
                 MakePersonalAlertsPublic(personalAlertIds);
+                MakeSaveSearchesPublic(personalSavedSearches);
             }
 
             SetPageShareProperties(
