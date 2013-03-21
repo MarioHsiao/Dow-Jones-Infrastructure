725e076447b2fa29f8eb91a3b85d40ccc0a997de
Dacosta, David <David.Dacosta@dowjones.com>
2013-03-21 21:25:08 +0000

    updated changes for ting and mamta
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C106666

--- trunk/src/DowJones.Tools.Charting/DowJones.Tools.Charting/Properties/AssemblyInfo.cs
+++ trunk/src/DowJones.Tools.Charting/DowJones.Tools.Charting/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.2.*")]
-[assembly: AssemblyFileVersion("1.0.2.*")]
+[assembly: AssemblyVersion("1.0.3.1")]
+[assembly: AssemblyFileVersion("1.0.3.1")]
 [assembly: InternalsVisibleTo("DowJones.Tools.Charting.Tests")]
