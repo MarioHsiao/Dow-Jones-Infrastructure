03e9dd31e0f17986fa1c19e5521b30a8038a0d3c
Goyal, Ruchi <Ruchi.Goyal@dowjones.com>
2013-04-01 14:30:45 +0000

    Adding and removing files from FNBLocal to sync the structure with FNB
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C107382

---
+++ Prototypes/GitHubTfsSyncApp/DemoCommits/DemoCommits/FNBLocal/build/javascripts/application.js
@@ -0,0 +1,6 @@
+//= require vendor/_jquery.mjs.nestedSortable
+//= require vendor/_jquery.placeholder.min
+
+//= require vendor/_bootstrap/bootstrap-button
+//= require vendor/_bootstrap/bootstrap-tab
+//= require vendor/_bootstrap/bootstrap-dropdown
