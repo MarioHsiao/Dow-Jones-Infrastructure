d1ccb32a35d238bb3544c0bc69bfa0196f07d582
Kandasamy, Yazhini <Yazhini.Kandasamy@dowjones.com>
2013-04-03 14:40:22 +0000

    Company Info: Display Provider Source info
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C107532

--- trunk/src/DowJones.Web.Mvc.UI.Components.Models/CompanyInfo/CompanyInfoModel.cs
+++ trunk/src/DowJones.Web.Mvc.UI.Components.Models/CompanyInfo/CompanyInfoModel.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace DowJones.Web.Mvc.UI.Components.CompanyInfo
 {
@@ -26,31 +27,31 @@ namespace DowJones.Web.Mvc.UI.Components.CompanyInfo
     public class Quote
     {
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets TickerSymbol.
         /// </summary>
         [JsonProperty("tickerSymbol")]
         public string TickerSymbol { get; set; }
 
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets StockExchange.
         /// </summary>
         [JsonProperty("stockExchange")]
         public string StockExchange { get; set; }
 
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets CurrentValue.
         /// </summary>
         [JsonProperty("currentValue")]
         public string CurrentValue { get; set; }
 
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets DifferenceValue.
         /// </summary>
         [JsonProperty("differenceValue")]
         public string DifferenceValue { get; set; }
         
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets DifferencePercentage.
         /// </summary>
         [JsonProperty("differencePercentage")]
         public string DifferencePercentage { get; set; }
@@ -59,75 +60,108 @@ namespace DowJones.Web.Mvc.UI.Components.CompanyInfo
     public class KeyFinancial
     {
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets ReportCurrency.
         /// </summary>
         [JsonProperty("reportCurrency")]
         public string ReportCurrency { get; set; }
 
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets Sales.
         /// </summary>
         [JsonProperty("sales")]
         public string Sales { get; set; }
 
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets SalesGrowth.
         /// </summary>
         [JsonProperty("salesGrowth")]
         public string SalesGrowth { get; set; }
 
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets Employees.
         /// </summary>
         [JsonProperty("employees")]
         public string Employees { get; set; }
         
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets EmployeesGrowth.
         /// </summary>
         [JsonProperty("employeesGrowth")]
         public string EmployeesGrowth { get; set; }
 
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets LastReportedAuditor.
         /// </summary>
         [JsonProperty("lastReportedAuditor")]
         public string LastReportedAuditor { get; set; } 
 
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets MarketCapitalization.
         /// </summary>
         [JsonProperty("marketCapitalization")]
         public string MarketCapitalization { get; set; }
 
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets MarketCapitalizationDate.
         /// </summary>
         [JsonProperty("marketCapitalizationDate")]
         public string MarketCapitalizationDate { get; set; }
 
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets NetIncome.
         /// </summary>
         [JsonProperty("netIncome")]
         public string NetIncome { get; set; }
 
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets NetProfitMargin.
         /// </summary>
         [JsonProperty("netProfitMargin")]
         public string NetProfitMargin { get; set; }
 
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets EPS.
         /// </summary>
         [JsonProperty("eps")]
         public string EPS { get; set; }
 
         /// <summary>
-        /// Gets or sets .
+        /// Gets or sets FiscalYearEndDate.
         /// </summary>
         [JsonProperty("fiscalYearEndDate")]
         public string FiscalYearEndDate { get; set; }
+        
+        /// <summary>
+        /// Gets or sets Provider.
+        /// </summary>
+        [JsonProperty("provider")]
+        public List<ProviderInfo> Provider { get; set; }
+    }
+
+    public class ProviderInfo
+    {
+        /// <summary>
+        /// Gets or sets ProviderCode.
+        /// </summary>
+        [JsonProperty("providerCode")]
+        public string ProviderCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets ProviderName.
+        /// </summary>
+        [JsonProperty("providerName")]
+        public string ProviderName { get; set; }
+
+        /// <summary>
+        /// Gets or sets ProviderTooltip.
+        /// </summary>
+        [JsonProperty("providerTooltip")]
+        public string ProviderTooltip { get; set; }
+        
+        /// <summary>
+        /// Gets or sets ProviderURL.
+        /// </summary>
+        [JsonProperty("providerURL")]
+        public string ProviderURL { get; set; }
     }
 }
