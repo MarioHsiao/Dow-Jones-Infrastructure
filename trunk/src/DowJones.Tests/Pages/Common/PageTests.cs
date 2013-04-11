3b2171e151c0328aa5a82b2998f430ee82f59e59
Dhange, Himmatsinh <Himmatsinh.Dhange@dowjones.com>
2013-04-11 01:57:43 +0000

    Refacored Page Sharing and Unsharing functions to reduce number of PAM calls.
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C108079

--- trunk/src/DowJones.Tests/Pages/Common/PageTests.cs
+++ trunk/src/DowJones.Tests/Pages/Common/PageTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Runtime.Serialization;
@@ -6,6 +7,9 @@ using System.Xml;
 using DowJones.DependencyInjection;
 using DowJones.DependencyInjection.Ninject;
 using DowJones.Infrastructure;
+using DowJones.Infrastructure.Common;
+using DowJones.Pages.Common;
+using DowJones.Session;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using GatewayPage = Factiva.Gateway.Messages.Assets.Pages.V1_0.NewsPage;
 
@@ -29,5 +33,37 @@ namespace DowJones.Pages
             var registry = AssemblyRegistry.CreateFromAssemblyReferences(Assembly.GetExecutingAssembly(), null);
             ServiceLocator.Initialize(new NinjectServiceLocatorFactory(registry));
         }
+
+        [TestMethod]
+        public void PublishPage()
+        {
+            IControlData controlData = new ControlData();
+            //controlData.UserID = "naresh4";
+            //controlData.UserPassword = "passwd";
+            //controlData.ProductID = "16";
+            controlData.SessionID = "27139ZzZKJHEQT2CAAAGUAYAAAAAIFZJAAAAAABSGAYTGMBUGEYDEMJUGY2TAMBT";
+
+            var pageAssetMgr = new PageAssetsManager(controlData, null, new Preferences.Preferences(), new Product("snapshot", ""));
+            var list = new List<IShareAssets>();
+            pageAssetMgr.PublishPage("28615", list);
+           
+
+        }
+
+        [TestMethod]
+        public void UnpublishPage()
+        {
+            IControlData controlData = new ControlData();
+            //controlData.UserID = "naresh4";
+            //controlData.UserPassword = "passwd";
+            //controlData.ProductID = "16";
+            controlData.SessionID = "27139ZzZKJHEQT2CAAAGUAYAAAAAIFZJAAAAAABSGAYTGMBUGEYDEMJUGY2TAMBT";
+
+            var pageAssetMgr = new PageAssetsManager(controlData, null, new Preferences.Preferences(), new Product("snapshot", ""));
+            var list = new List<IShareAssets>();
+            pageAssetMgr.UnpublishPage("28615", list);
+
+        }
+
     }
 }
