799064ea5f195405bd05dfa5b315e1320466b447
Hua, Lei <Lei.Hua@dowjones.com>
2013-04-29 15:23:04 +0000

    fork create syndication items
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C109189

--- trunk/src/DowJones.Infrastructure/Managers/Rss/RssManager.cs
+++ trunk/src/DowJones.Infrastructure/Managers/Rss/RssManager.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using DowJones.DependencyInjection;
 using DowJones.Properties;
 using Factiva.Gateway.Messages.Assets.Item.V1_0;
@@ -18,6 +21,9 @@ namespace DowJones.Managers.Rss
 
         // If we ask for more, the service will throw error
         private readonly int MAX_ITEMS_TO_GET = Convert.ToInt32(ConfigurationManager.AppSettings["RSS_FEEDS_MAX_LIMIT"] ?? "240");
+
+        // Number of concurrent operations to create syndication items
+        private readonly int MAX_PARALLEL_TASKS_TO_CREATE_SYNDICATION_ITEMS = Convert.ToInt32(ConfigurationManager.AppSettings["MAX_PARALLEL_TASKS_TO_CREATE_SYNDICATION_ITEMS"] ?? "-1");
 
         public CreateSyndicationItemExResponse CreateSyndicationItem(RssItem rssItem)
         {
@@ -58,44 +64,62 @@ namespace DowJones.Managers.Rss
         {
             if (lstRssItem == null || lstRssItem.Count() == 0) return null;
 
-            var lstResponse = new List<CreateSyndicationItemExResponse>();
-            foreach (var rssItem in lstRssItem)
+            //LeiH: change to multi-tasking for performnace gain
+            //var _cancellation = new CancellationTokenSource(); //Todo: in case abort is needed
+            var po = new ParallelOptions
             {
+                //CancellationToken = _cancellation.Token,
+                MaxDegreeOfParallelism = MAX_PARALLEL_TASKS_TO_CREATE_SYNDICATION_ITEMS
+            };
+            var cdResponse = new ConcurrentDictionary<int, CreateSyndicationItemExResponse>();
+            var count = lstRssItem.Count();
+            Parallel.For(0, count, po, (i, loopState)=>
+                                                    {
+                                                        var rssItem = lstRssItem.ElementAt(i);
+                                                     //if (_cancellation.IsCancellationRequested)
+                                                     //    loopState.Stop();
 
-                //TODO: Add logic to fork multiple tasks
+                                                     CategoryCollection categoryCollection = null;
+                                                     if (!string.IsNullOrEmpty(rssItem.Category))
+                                                     {
+                                                         categoryCollection = new CategoryCollection {rssItem.Category};
+                                                     }
+                                                     var itemEx = new SyndicationItemEx()
+                                                                      {
+                                                                          Properties = new SyndicationItemProperties()
+                                                                                           {
+                                                                                               SyndicationItemType = SyndicationItemType.RSS,
+                                                                                               Name = rssItem.Name,
+                                                                                               Value = rssItem.Url,
+                                                                                               Aggregation = GetAggregation(rssItem.AggregationDays),
+                                                                                               CategoryCollection = categoryCollection
+                                                                                           },
+                                                                          ShareProperties = new ShareProperties
+                                                                                                {
+                                                                                                    AccessControlScope = AccessControlScope.Personal,
+                                                                                                    AssignedScope = ShareScope.Personal,
+                                                                                                    ListingScope = ShareScope.Personal,
+                                                                                                    SharePromotion = ShareScope.Personal
+                                                                                                }
+                                                                      };
 
-                CategoryCollection categoryCollection = null;
-                if (!string.IsNullOrEmpty(rssItem.Category))
+                                                     var request = new CreateSyndicationItemExRequest()
+                                                                       {
+                                                                           SyndicationItemEx = itemEx
+                                                                       };
+                                                        var res = SyndicationAggregationService.CreateSyndicationItemEx(ControlData, request);
+                                                     cdResponse.AddOrUpdate(i, res, (key, oldvalue) => res);
+                                                 });
+            if (!cdResponse.IsEmpty)
+            {
+                var lstResponse = new List<CreateSyndicationItemExResponse>(count);
+                for (var i = 0; i < count; i++)
                 {
-                    categoryCollection = new CategoryCollection {rssItem.Category};
+                    lstResponse.Add(cdResponse[i]);
                 }
-                var itemEx = new SyndicationItemEx()
-                                 {
-                                     Properties = new SyndicationItemProperties()
-                                                      {
-                                                          SyndicationItemType = SyndicationItemType.RSS,
-                                                          Name = rssItem.Name,
-                                                          Value = rssItem.Url,
-                                                          Aggregation = GetAggregation(rssItem.AggregationDays),
-                                                          CategoryCollection = categoryCollection
-                                                      },
-                                     ShareProperties = new ShareProperties
-                                                           {
-                                                               AccessControlScope = AccessControlScope.Personal,
-                                                               AssignedScope = ShareScope.Personal,
-                                                               ListingScope = ShareScope.Personal,
-                                                               SharePromotion = ShareScope.Personal
-                                                           }
-                                 };
-
-                var request = new CreateSyndicationItemExRequest()
-                                  {
-                                      SyndicationItemEx = itemEx
-                                  };
-
-                lstResponse.Add(SyndicationAggregationService.CreateSyndicationItemEx(ControlData, request));
+                return lstResponse;
             }
-            return lstResponse;
+            return null;
         }
 
         public GetSyndicationItemExListResponse GetSyndicationItemList()
