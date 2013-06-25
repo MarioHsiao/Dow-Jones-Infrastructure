d1201441311b37d496f0df23070ebedd2b54229c
Dacosta, David <David.Dacosta@dowjones.com>
2013-06-25 14:11:05 +0000

    initial upload of site constructs
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C118001

---
+++ Prototypes/DowJones.Prod.X.Prototype/DowJones.Prod.X.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DowJones.Prod.X.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
