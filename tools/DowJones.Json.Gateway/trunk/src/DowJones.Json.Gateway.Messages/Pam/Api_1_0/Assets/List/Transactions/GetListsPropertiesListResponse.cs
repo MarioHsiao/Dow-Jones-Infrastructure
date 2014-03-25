06a41203a7a889fc9c155e773a9c5a969285e745
Dacosta, David <David.Dacosta@dowjones.com>
2014-03-25 12:54:57 +0000

    updated transactions inheritance
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C130784

--- tools/DowJones.Json.Gateway/trunk/src/DowJones.Json.Gateway.Messages/Pam/Api_1_0/Assets/List/Transactions/GetListsPropertiesListResponse.cs
+++ tools/DowJones.Json.Gateway/trunk/src/DowJones.Json.Gateway.Messages/Pam/Api_1_0/Assets/List/Transactions/GetListsPropertiesListResponse.cs
@@ -1,9 +1,10 @@
 using System.Runtime.Serialization;
+using DowJones.Json.Gateway.Interfaces;
 
 namespace DowJones.Json.Gateway.Messages.Pam.Api_1_0.Assets.List.Transactions
 {
     [DataContract(Name = "GetListsPropertiesListResponse", Namespace = "")]
-    public class GetListsPropertiesListResponse
+    public class GetListsPropertiesListResponse : IJsonRestResponse
     {
         [DataMember(Name = "listPropertiesItemCollection", IsRequired = true, EmitDefaultValue = false)]
         public ListPropertiesItemCollection ListPropertiesItemCollection { get; set; }
