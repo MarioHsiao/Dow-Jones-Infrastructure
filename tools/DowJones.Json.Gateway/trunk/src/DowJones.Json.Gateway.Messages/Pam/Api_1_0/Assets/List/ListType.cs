50fbb1ee7d635791c1a2f73c08d50d6f61a82a8c
Dacosta, David <David.Dacosta@dowjones.com>
2014-03-28 19:01:55 +0000

    updated listtype to new values
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C131096

--- tools/DowJones.Json.Gateway/trunk/src/DowJones.Json.Gateway.Messages/Pam/Api_1_0/Assets/List/ListType.cs
+++ tools/DowJones.Json.Gateway/trunk/src/DowJones.Json.Gateway.Messages/Pam/Api_1_0/Assets/List/ListType.cs
@@ -5,14 +5,14 @@ namespace DowJones.Json.Gateway.Messages.Pam.Api_1_0.Assets.List
     [DataContract(Name = "ListType", Namespace = "")]
     public enum ListType
     {
-        [EnumMember(Value = "AuthorList")] AuthorList = 0,
+        [EnumMember(Value = "AuthorList")] AuthorList = 1,
 
-        [EnumMember(Value = "IndustryList")] IndustryList = 1,
+        [EnumMember(Value = "IndustryList")] IndustryList = 2,
 
-        [EnumMember(Value = "RegionList")] RegionList = 2,
+        [EnumMember(Value = "RegionList")] RegionList = 3,
 
-        [EnumMember(Value = "SubjectList")] SubjectList = 3,
+        [EnumMember(Value = "SubjectList")] SubjectList = 4,
 
-        [EnumMember(Value = "ExecutiveList")] ExecutiveList = 4,
+        [EnumMember(Value = "ExecutiveList")] ExecutiveList = 5,
     }
 }
