85803d026868f0150297444334706bc460f8d333
Dacosta, David <David.Dacosta@dowjones.com>
2014-05-07 17:49:50 +0000

    implementation of packages
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C134041

--- tools/DowJones.Json.Gateway/trunk/src/DowJones.Json.Gateway/Properties/AssemblyInfo.cs
+++ tools/DowJones.Json.Gateway/trunk/src/DowJones.Json.Gateway/Properties/AssemblyInfo.cs
@@ -23,18 +23,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("aad532ed-57bf-497a-ba3c-6a1a79e28085")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
 [assembly: InternalsVisibleTo("DowJones.Json.Gateway.Tests")]
 [assembly: InternalsVisibleTo("DowJones.Json.Gateway.Messages")]
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
