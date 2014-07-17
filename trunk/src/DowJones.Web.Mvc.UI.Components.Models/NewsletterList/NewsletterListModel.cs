1abe5abf41f6ebf25badf3ee0686b2ba52a640d5
Hua, Lei <Lei.Hua@dowjones.com>
2014-07-17 18:13:49 +0000

    use data model defined in infrastructure
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C142445

--- trunk/src/DowJones.Web.Mvc.UI.Components.Models/NewsletterList/NewsletterListModel.cs
+++ trunk/src/DowJones.Web.Mvc.UI.Components.Models/NewsletterList/NewsletterListModel.cs
@@ -10,34 +10,13 @@ namespace DowJones.Web.Mvc.UI.Components.NewsletterList
 {
     public class NewsletterListModel: ViewComponentModel
     {
-        [JsonProperty("newsletters")]
-        public IEnumerable<Newsletter> Newsletters { get; set; }
-
         [ClientData]
         public NewsletterListDataResult Result { get; set; }
 
         public NewsletterListModel(NewsletterListDataResult dataResult = null)
         {
             Result = dataResult;
-            Newsletters = Enumerable.Empty<Newsletter>();
         }
     }
 
-    public class Newsletter
-    {
-        [JsonProperty("code")]
-        public string Code { get; set; }
-
-        [JsonProperty("id")]
-        public long Id { get; set; }
-
-        [JsonProperty("name")]
-        public string Name { get; set; }
-
-        [JsonProperty("lastModifiedDate")]
-        public DateTime LastModifiedDate { get; set; }
-
-        [JsonProperty("lastModifiedDateDescriptor")]
-        public string LastModifiedDateDescriptor { get; set; }
-    }
 }
