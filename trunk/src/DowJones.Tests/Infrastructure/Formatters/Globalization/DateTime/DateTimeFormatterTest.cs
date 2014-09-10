bf72ac84199b14f7e44d7a1e3ef651c2ff28e460
Kolli, Naga swetha <NAGASWETHA.KOLLI@dowjones.com>
2014-09-10 21:13:17 +0000

    Add unit tests for date time formatter - interface language french
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C144593

---
+++ trunk/src/DowJones.Tests/Infrastructure/Formatters/Globalization/DateTime/DateTimeFormatterTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DowJones.Formatters.Globalization.DateTime;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DowJones.Infrastructure.Formatters.Globalization.DateTime
+{
+    [TestClass]
+    public class DateTimeFormatterTest
+    {
+        private DateTimeFormatter _dateTimeFormatter;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _dateTimeFormatter = new DateTimeFormatter("fr");
+        }
+
+        [TestMethod]
+        public void Full_Date_Format_In_French_Should_Be_d_MMMM_YYYY()
+        {
+            string date = "08/11/2014";
+            System.DateTime dt = Convert.ToDateTime(date);
+            var formattedDate = _dateTimeFormatter.FormatFullDate(dt, true);
+            Assert.AreEqual(formattedDate, "11 août 2014", "Formatted date in french");
+        }
+    }
+}
