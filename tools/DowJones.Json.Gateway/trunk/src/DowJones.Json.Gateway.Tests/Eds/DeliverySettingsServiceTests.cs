cff693536fd2970044a00a886cca3054e75b5c4d
Apostolov, Valentin <Valentin.Apostolov@dowjones.com>
2014-11-14 13:18:01 +0000

    GetDeliverySettingsRequest Unit Tests
    git-tfs-id: [http://tfs2012.dowjones.net:8080/tfs/DefaultCollection]$/Dow Jones Infrastructure;C146371

--- tools/DowJones.Json.Gateway/trunk/src/DowJones.Json.Gateway.Tests/Eds/DeliverySettingsServiceTests.cs
+++ tools/DowJones.Json.Gateway/trunk/src/DowJones.Json.Gateway.Tests/Eds/DeliverySettingsServiceTests.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Globalization;
 using System.Threading;
-using DowJones.Json.Gateway.Common;
 using DowJones.Json.Gateway.Converters;
 using DowJones.Json.Gateway.Exceptions;
 using DowJones.Json.Gateway.Extensions;
 using DowJones.Json.Gateway.Interfaces;
+using DowJones.Json.Gateway.Messages.Eds.Api_1_0.DeliverySettings;
 using DowJones.Json.Gateway.Messages.Eds.Api_1_0.DeliverySettings.Transactions;
 using DowJones.Json.Gateway.Messages.Eds.Api_1_0.Ode;
 using DowJones.Json.Gateway.Messages.Pam.Api_1_0.Assets.List;
@@ -13,9 +13,13 @@ using DowJones.Json.Gateway.Messages.Pam.Api_1_0.Assets.List.Transactions;
 using DowJones.Json.Gateway.Messages.Pam.Api_1_0.Sharing;
 using DowJones.Json.Gateway.Tests.Core;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Environment = DowJones.Json.Gateway.Common.Environment;
 using DowJones.Json.Gateway.Messages.Eds.Api_1_0.Ode.Transactions;
 using System.Collections.Generic;
+using Formatting = Newtonsoft.Json.Formatting;
+using JsonSerializer = DowJones.Json.Gateway.Common.JsonSerializer;
 
 namespace DowJones.Json.Gateway.Tests.Eds
 {
@@ -47,6 +51,25 @@ namespace DowJones.Json.Gateway.Tests.Eds
                 Console.WriteLine(response.Error.Message);
                 Assert.Fail(string.Concat("failed w/rc:= ", response.ReturnCode.ToString(CultureInfo.InvariantCulture)));
             }
+        }
+
+        [TestMethod]
+        public void Verify_Request_Properties_Names_Are_Properly_Serialized()
+        {
+            GetDeliverySettingsRequest request = new GetDeliverySettingsRequest()
+            {
+                DeliveryId = "30029078",
+                DeliveryType = DeliveryType.C,
+                ReturnType = ReturnType.Full
+            };
+
+            string serializedObject = request.ToJson(new JsonDotNetJsonConverter());
+            if (serializedObject == null) throw new ArgumentNullException("serializedObject");
+
+            Assert.AreNotEqual(serializedObject.IndexOf("returnType"), -1);
+            Assert.AreNotEqual(serializedObject.IndexOf("deliveryId"), -1);
+            Assert.AreNotEqual(serializedObject.IndexOf("deliveryType"), -1);
+
         }
 
         private static void UpdateRoutingData(IRoutingData routingData)
